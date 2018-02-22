50a03753452da39e2bce162497432234bccb0c36
Nils Oberg <noberg@illinois.edu>
2018-02-22 12:35:02 -0600

    Fixed job ID name bug.

--- generatedata.pl
+++ generatedata.pl
@@ -388,6 +388,7 @@ $logDir = "" if not -d $logDir;
 my %schedArgs = (type => $schedType, queue => $queue, resource => [1, 1, "35gb"], dryrun => $dryrun);
 $schedArgs{output_base_dirpath} = $logDir if $logDir;
 my $S = new EFI::SchedulerApi(%schedArgs);
+my $jobNamePrefix = $jobId ? $jobId . "_" : ""; 
 
 my $scriptDir = "$baseOutputDir/scripts";
 mkdir $scriptDir;
@@ -423,7 +424,7 @@ if ($pfam or $ipro or $ssf or $gene3d or ($fastaFile=~/\w+/ and !$taxid) or $acc
     my $randomFractionOpt = $randomFraction ? "-random-fraction" : "";
     $B->addAction("$efiEstTools/getsequence-domain.pl -domain $domain $fastaFileOption $userHeaderFileOption -ipro $ipro -pfam $pfam -ssf $ssf -gene3d $gene3d -accession-id $accessionId $accessionFileOption $noMatchFile -out $outputDir/allsequences.fa $maxSeqOpt -fraction $fraction $randomFractionOpt -accession-output $accOutFile -error-file $errorFile $seqCountFileOption $unirefOption $unirefExpandOption -config=$configFile");
     $B->addAction("$efiEstTools/getannotations.pl -out $outputDir/struct.out -fasta $outputDir/allsequences.fa $userHeaderFileOption -config=$configFile");
-    $B->jobName("${jobNamePrefix}_initial_import");
+    $B->jobName("${jobNamePrefix}initial_import");
     $B->renderToFile("$scriptDir/initial_import.sh");
 
     # Submit and keep the job id for next dependancy
@@ -450,7 +451,7 @@ if ($pfam or $ipro or $ssf or $gene3d or ($fastaFile=~/\w+/ and !$taxid) or $acc
         $userHeaderFile=~s/^-userdat //;
         $B->addAction("cat $userHeaderFile >> struct.out");
     }
-    $B->jobName("${jobNamePrefix}_initial_import");
+    $B->jobName("${jobNamePrefix}initial_import");
     $B->renderToFile("$scriptDir/initial_import.sh");
 
     $importjob = $S->submit("$scriptDir/initial_import.sh");
@@ -494,7 +495,7 @@ if ($cdHitOnly) {
     }
     $B->addAction("touch  $outputDir/1.out.completed");
 
-    $B->jobName("${jobNamePrefix}_cdhit");
+    $B->jobName("${jobNamePrefix}cdhit");
     $B->renderToFile("$scriptDir/cdhit.sh");
     $cdhitjob = $S->submit("$scriptDir/cdhit.sh");
     chomp $cdhitjob;
@@ -535,7 +536,7 @@ CMDS
 } else {
     $B->addAction("cp $outputDir/allsequences.fa $outputDir/sequences.fa");
 }
-$B->jobName("${jobNamePrefix}_multiplex");
+$B->jobName("${jobNamePrefix}multiplex");
 $B->renderToFile("$scriptDir/multiplex.sh");
 
 $muxjob = $S->submit("$scriptDir/multiplex.sh");
@@ -552,7 +553,7 @@ $B = $S->getBuilder();
 $B->dependency(0, @muxjobline[0]);
 $B->addAction("mkdir -p $fracOutputDir");
 $B->addAction("$efiEstTools/splitfasta.pl -parts $np -tmp $fracOutputDir -source $outputDir/sequences.fa");
-$B->jobName("${jobNamePrefix}_fracfile");
+$B->jobName("${jobNamePrefix}fracfile");
 $B->renderToFile("$scriptDir/fracfile.sh");
 
 $fracfilejob = $S->submit("$scriptDir/fracfile.sh");
@@ -577,7 +578,7 @@ if ($blast eq 'diamond' or $blast eq 'diamondsensitive') {
 } else {
     $B->addAction("formatdb -i sequences.fa -n database -p T -o T ");
 }
-$B->jobName("${jobNamePrefix}_createdb");
+$B->jobName("${jobNamePrefix}createdb");
 $B->renderToFile("$scriptDir/createdb.sh");
 
 $createdbjob = $S->submit("$scriptDir/createdb.sh");
@@ -637,7 +638,7 @@ $B->addAction("    echo \"BLAST failed; likely due to file format.\"");
 $B->addAction("    echo $OUT > $outputDir/blast.failed");
 $B->addAction("    exit 1");
 $B->addAction("fi");
-$B->jobName("${jobNamePrefix}_blast-qsub");
+$B->jobName("${jobNamePrefix}blast-qsub");
 $B->renderToFile("$scriptDir/blast-qsub.sh");
 
 $B->jobArray("");
@@ -663,7 +664,7 @@ $B->addAction("    exit 1");
 $B->addAction("fi");
 #$B->addAction("rm  $blastOutputDir/blastout-*.tab");
 #$B->addAction("rm  $fracOutputDir/fracfile-*.fa");
-$B->jobName("${jobNamePrefix}_catjob");
+$B->jobName("${jobNamePrefix}catjob");
 $B->renderToFile("$scriptDir/catjob.sh");
 $catjob = $S->submit("$scriptDir/catjob.sh");
 chomp $catjob;
