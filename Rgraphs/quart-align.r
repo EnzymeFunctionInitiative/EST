29a6fee1f2d9e499d9e05d128735df008ad6cd62
Nils Oberg <noberg@illinois.edu>
2018-12-11 12:08:09 -0600

    Fixed bugs in graphing code.

--- Rgraphs/quart-align.r
+++ Rgraphs/quart-align.r
@@ -27,15 +27,14 @@ if (type == "hdf5") {
 } else {
 
     data_dir = data_file
-    data_files=list.files(path = data_dir, pattern = "align")
+    data_files = list.files(path = data_dir, pattern = "align")
+    a_scores = sapply(data_files, function(x) { as.numeric(substr(x, nchar(x)-5+1, nchar(x))) }, USE.NAMES = FALSE)
 
     start = as.integer(args[4])
     stop = as.integer(args[5])
     maxy = as.integer(args[6])
 
-    print(paste("range",start,stop))
     newdata = t(rep(NA,length(data_files)))
-    #colnames(data)<-start:stop
 
     arg_offset = 6
 }
@@ -55,20 +54,23 @@ if (length(args) > arg_offset+2) {
 }
 
 
-print(paste(start,",",stop,",",maxy))
 png(png_file, width=im_width, height=im_height, type="cairo")
+
+num_boxes <- stop - start + 1
+if (length(a_scores) > 0) {
+    num_boxes <- length(a_scores)
+}
+step_size <- ceiling(10 / (im_width / num_boxes))
 
 bar_step_size <- 1
 whisk_color <- "gray40"
-if (im_width < 1000) {
+if (im_width < 1000 && step_size > 1) {
     bar_step_size = 3
     whisk_color <- "gray60"
 }
 
-num_boxes <- stop - start + 1
-step_size <- ceiling(10 / (im_width / num_boxes))
-box_range <- seq(start, num_boxes + step_size, step_size)
-bars_to_use <- seq(start, num_boxes, bar_step_size)
+box_range <- seq(start, start + num_boxes + step_size, step_size)
+bars_to_use <- seq(start, start + num_boxes, bar_step_size)
 
 boxplot(newdata,
         main = paste("Alignment Length vs Alignment Score", jobnum),
@@ -77,21 +79,24 @@ boxplot(newdata,
         ylab = "Alignment Length",
         xlab = "Alignment Score",
         ylim = range(0,maxy),
+        xlim = range(bars_to_use[0], bars_to_use[-1]),
         xaxt = "n",
         frame = F)
 
 for (i in bars_to_use){
-    key=i #i+start-1
-    #so this is an array,has to be rotated
+    key = i
     if (type == "hdf5") {
         print(paste0("/align/",key))
         newdata = t(h5read(data_file,paste0("/align/",key)))
     } else {
-   #     key=i-start+1
         idx = i - start + 1
         full_path = paste(data_dir,"/",data_files[idx],sep='')
         print(full_path)
-        newdata = read.table(full_path, header=TRUE, sep="\t", check.names = FALSE)
+        if (!file.exists(full_path)) {
+            newdata = vector()
+        } else {
+            newdata = read.table(full_path, header=TRUE, sep="\t", check.names = FALSE)
+        }
     }
     if (length(newdata) == 0)
         next
