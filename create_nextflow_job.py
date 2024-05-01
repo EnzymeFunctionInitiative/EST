29ad6fcdab3c0cd7f6294890a13bde5fb224831f
Hunter DeMeyer <demeyer3@illinois.edu>
2024-05-01 09:29:05 -0500

    Parameterzie additional options

--- create_nextflow_job.py
+++ create_nextflow_job.py
@@ -1,13 +1,36 @@
 import argparse
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
+import sys
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Render templates for nextflow job run")
-    parser.add_argument("--job-id", type=int, default=29897, help="Job ID number to rerun using nextflow")
+    parser.add_argument("--workflow-def", type=str, default="est.nf", help="Location of the nextflow workflow file")
+    parser.add_argument("--est-dir", type=str, default=".", help="Directory where EST scripts are")
+    parser.add_argument("--job-id", type=int, required=True, help="Job ID number to rerun using nextflow")
     parser.add_argument("--output-dir", type=str, required=True, help="Where to store outputs from run")
-    parser.add_argument("--template-dir", type=str, required=True, help="Where the templates are stored")
-    return parser.parse_args()
+    parser.add_argument("--duckdb-threads", type=int, default=1, help="Number of threads to let DuckDB use")
+    parser.add_argument("--duckdb-mem", type=str, default="4GB", help="Soft limit on RAM for DuckDB")
+    parser.add_argument("--fasta-shards", type=int, default=64, help="Number of pieces to break the FASTA file into")
+    parser.add_argument("--blast-matches", type=int, default=250, help="Number of matches for BLAST to return for each query")
+    parser.add_argument("--config-path", type=str, required=True, help="Path to nextflow config file for run")
+    args = parser.parse_args()
+
+    fail = False
+    if not os.path.exists(args.est_dir):
+        print(f"est dir '{args.est_dir}' does not exist", file=sys.stderr)
+        fail = True
+    if not os.path.exists(args.workflow_def):
+        print(f"workflow definition '{args.workflow_def}' does not exist", file=sys.stderr)
+        fail = True
+    if not os.path.exists(args.config_path):
+        print(f"config path '{args.config_path}' does not exist", file=sys.stderr)
+        fail = True
+    
+    if fail:
+        exit(1)
+    else:
+        return args
 
 
 if __name__ == "__main__":
@@ -15,15 +38,31 @@ if __name__ == "__main__":
     args.output_dir = os.path.join(args.output_dir, f"rerun_{args.job_id}")
     os.makedirs(args.output_dir, exist_ok=True)
 
-    env = Environment(loader=FileSystemLoader(args.template_dir), autoescape=select_autoescape())
+    env = Environment(loader=FileSystemLoader(f"{args.est_dir}/templates"), autoescape=select_autoescape())
     params_template = env.get_template("params.yml.jinja")
-    sh_template = env.get_template("run_nextflow.sh.jinja")
+    sh_template = env.get_template("run_nextflow_slurm.sh.jinja")
 
-    params = params_template.render(est_dir="/home/a-m/demeyer3/EST", job_dir=f"/private_stores/gerlt/jobs/dev/est/{args.job_id}", output_dir=args.output_dir)
+    params = params_template.render(est_dir=args.est_dir, 
+                                    job_dir=f"/private_stores/gerlt/jobs/dev/est/{args.job_id}", 
+                                    output_dir=args.output_dir,
+                                    duckdb_threads=args.duckdb_threads,
+                                    duckdb_mem=args.duckdb_mem,
+                                    fasta_shards=args.fasta_shards,
+                                    blast_matches=args.blast_matches,
+                                    job_id=args.job_id)
     params_output = os.path.join(args.output_dir, "params.yml")
     with open(params_output, "w") as f:
         f.write(params)
-    
-    submission_script = sh_template.render(workflow_definition="/home/a-m/demeyer3/EST/endstages.nf", params_file=params_output, report_file="report.html", timeline_file="timeline.html",output_dir=args.output_dir,job_id=args.job_id)
-    with open(os.path.join(args.output_dir, "run_nextflow.sh"), "w") as f:
+    print(f"Wrote params to {params_output}")
+
+    submission_script = sh_template.render(workflow_definition=args.workflow_def, 
+                                           params_file=params_output,
+                                           report_file="report.html",
+                                           timeline_file="timeline.html",
+                                           output_dir=args.output_dir,
+                                           job_id=args.job_id,
+                                           config_path=args.config_path)
+    submission_script_output = os.path.join(args.output_dir, "run_nextflow.sh")
+    with open(submission_script_output, "w") as f:
         f.write(submission_script)
+    print(f"Wrote submission script to {submission_script_output}")
--- est.nf
+++ est.nf
@@ -15,8 +15,6 @@ process create_blast_db {
         path "database.*"
         val "database"
     """
-    module load efidb/ip98
-    module load efiest/devlocal
     formatdb -i ${fasta_file} -n database -p T -o T
     """
 }
@@ -39,10 +37,10 @@ process all_by_all_blast {
     output:
         path "${frac}.tab.parquet"
     """
-    module load efidb/ip98
-    module load efiest/devlocal
-    module load Python
-    module load efiest/python_est_1.0
+    # module load efidb/ip98
+    # module load efiest/devlocal
+    # module load Python
+    # module load efiest/python_est_1.0
     blastall -p blastp -i $frac -d $blast_db_name -m 8 -e 1e-5 -b ${params.num_blast_matches} -o ${frac}.tab
     python ${params.est_dir}/blastreduce/transcode_blast.py --blast-output ${frac}.tab
     """
@@ -55,8 +53,8 @@ process blastreduce_transcode_fasta {
         path "${fasta_file.getName()}.parquet"
 
     """
-    module load Python
-    module load efiest/python_est_1.0
+    # module load Python
+    # module load efiest/python_est_1.0
     python ${params.est_dir}/blastreduce/transcode_fasta_lengths.py --fasta $fasta_file --output ${fasta_file.getName()}.parquet
     """
 }
@@ -71,17 +69,16 @@ process blastreduce {
         path "1.out.parquet"
 
     """
-    module load Python
-    module load efiest/python_est_1.0
-    module load efidb/ip98
-    python ${params.est_dir}/blastreduce/render_sql_template.py --blast-output $blast_files  --sql-template ${params.est_dir}/blastreduce/reduce-template.sql --fasta-length-parquet $fasta_length_parquet --duckdb-memory-limit ${params.duckdb_memory_limit} --sql-output-file reduce.sql
-    module load DuckDB
+    # module load Python
+    # module load efiest/python_est_1.0
+    # module load efidb/ip98
+    python ${params.est_dir}/blastreduce/render_sql_template.py --blast-output $blast_files  --sql-template ${params.est_dir}/templates/reduce-template.sql --fasta-length-parquet $fasta_length_parquet --duckdb-memory-limit ${params.duckdb_memory_limit} --sql-output-file reduce.sql
+    # module load DuckDB
     duckdb < reduce.sql
     """
 }
 
 process visualize_blast {
-    publishDir params.final_output_dir, mode: 'copy'
     input:
         path blast_parquet
     output:
@@ -91,9 +88,25 @@ process visualize_blast {
         path 'evalue.tab'
 
     """
-    module load Python
-    module load efiest/python_est_1.0
-    python ${params.est_dir}/visualization/process_blast_results.py --blast-output $blast_parquet --job-id 1 --length-plot-filename length --pident-plot-filename pident --edge-hist-filename edge --evalue-tab-filename evalue.tab --proxies sm:48
+    # module load Python
+    # module load efiest/python_est_1.0
+    python ${params.est_dir}/visualization/process_blast_results.py --blast-output $blast_parquet --job-id $job_id --length-plot-filename length --pident-plot-filename pident --edge-hist-filename edge --evalue-tab-filename evalue.tab --proxies sm:48
+    """
+}
+
+process finalize_ouptut {
+    publishDir params.final_output_dir, mode: 'copy'
+    input:
+        path blast_output
+        path length_boxplot
+        path pident_boxplot
+        path edge_histo
+        path evalue_tab
+    output:
+        path 'output.tar'
+    """
+    python finalize/transcode_blast_parquet.py --blast-parquet $blast_output
+    # tar -cf output.tar 1.out $length_boxplot $pident_boxplot $edge_histo $evalue_tab
     """
 }
 
@@ -102,9 +115,7 @@ workflow {
     fasta_file = import_sequences(params.fasta_file)
 
     // step 2: create blastdb and frac seq file 
-    // chunk_size = (int) Math.ceil(num_fasta_records / 256)
     fasta_lengths_parquet = blastreduce_transcode_fasta(fasta_file)
-    // fasta_fractions = fasta_file | splitFasta(size: params.blast_shard_file_size, file: true)
     fasta_fractions = split_fasta(fasta_file)
     blastdb = create_blast_db(fasta_file)
 
@@ -113,5 +124,8 @@ workflow {
     reduced_blast_parquet = blastreduce(blast_fractions, fasta_lengths_parquet)
 
     // step 4: visualize and save
-    visualize_blast(reduced_blast_parquet)
+    plots = visualize_blast(reduced_blast_parquet)
+
+    // step 5: copy files to output dir
+    finalize_ouptut(reduced_blast_parquet, plots)
 }
