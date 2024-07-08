c0b4b5c333c9db832d4d15a4a2bcd520aa899297
Hunter DeMeyer <demeyer3@illinois.edu>
2024-07-08 13:24:03 -0500

    Refactor mode tests out of main script

---
+++ tests/runtests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+TEST_RESULTS_DIR=test_results
+
+if [[ ! -e ssn.nf || ! -e est.nf ]]; then
+    echo "Run this script from the repository root"
+    exit 1
+fi
+
+if [[ ! -e smalldata || ! -d smalldata ]]; then
+    echo "Test data directory not found, attempting to download"
+    mkdir smalldata
+    curl -o smalldata/data.tar.gz https://efi.igb.illinois.edu/downloads/sample_data/kb_test_all.tar.gz
+    tar xzf smalldata/data.tar.gz -C smalldata/
+fi
+
+source efi-env/bin/activate
+
+rm -rf $TEST_RESULTS_DIR
+mkdir $TEST_RESULTS_DIR
+
+for file in $(ls tests/modules); do
+    echo "================================================================================"
+    echo "Executing test in '$file'"
+    bash "tests/modules/$file" $TEST_RESULTS_DIR
+done;
