fc96f69ce7f40f12e92b208257845126379f6227
Hunter DeMeyer <demeyer3@illinois.edu>
2024-07-09 13:26:03 -0500

    Add parameter/job creation scripts

--- create_nextflow_job.py
+++ create_nextflow_job.py
@@ -5,6 +5,7 @@ import os
 
 import create_est_nextflow_params
 import create_ssn_nextflow_params
+import create_colorssn_nextflow_params
 
 def check_args(args: argparse.Namespace) -> argparse.Namespace:
     """
@@ -27,10 +28,15 @@ def check_args(args: argparse.Namespace) -> argparse.Namespace:
         args.workflow_def = os.path.abspath(args.workflow_def)
         args.config_path = os.path.abspath(args.config_path)
 
-    if args.command == "est":
+    if args.command == "colorssn":
+        args = create_colorssn_nextflow_params.check_args(args)
+    elif args.command == "est":
         args = create_est_nextflow_params.check_args(args)
-    else:
+    elif args.command == "ssn":
         args = create_ssn_nextflow_params.check_args(args)
+    else:
+        print(f"Job type '{args.command}' not known")
+        exit(1)
 
     return args
 
@@ -46,6 +52,10 @@ def create_parser() -> argparse.ArgumentParser:
     subparsers = parser.add_subparsers(dest="command")
 
     # add pipelines as subcommands
+    colorssn_parser = subparsers.add_parser("colorssn", help="Create a Color SSN pipeline job script")
+    colorssn_parser.add_argument("--workflow-def", default="colorssn.nf", help="Location of the Color SSN nextflow workflow file")
+    create_colorssn_nextflow_params.add_args(colorssn_parser)
+
     est_parser = subparsers.add_parser("est", help="Create an EST pipeline job script")
     est_parser.add_argument("--workflow-def", type=str, default="est.nf", help="Location of the EST nextflow workflow file")
     create_est_nextflow_params.add_args(est_parser)
@@ -66,7 +76,9 @@ if __name__ == "__main__":
     del args_dict["workflow_def"]
     del args_dict["templates_dir"]
     del args_dict["config_path"]
-    if args.command == "est":
+    if args.command == "colorssn":
+        params_output = create_colorssn_nextflow_params.render_params(**args_dict)
+    elif args.command == "est":
         params_output = create_est_nextflow_params.render_params(**args_dict)
     elif args.command == "ssn":
         params_output = create_ssn_nextflow_params.render_params(**args_dict)
