d294e49ea87878b5a8500d8914513e95e42efcdd
Hunter DeMeyer <demeyer3@illinois.edu>
2024-07-09 16:46:11 -0500

    Update subcommand spelling

--- tests/modules/est_accession.sh
+++ tests/modules/est_accession.sh
@@ -7,7 +7,7 @@ OUTPUT_DIR="$TEST_RESULTS_DIR/test_results_sequence_blast"
 
 rm -rf $OUTPUT_DIR
 
-python create_est_nextflow_params.py accession --output-dir $OUTPUT_DIR --efi-config smalldata/efi.config --fasta-db smalldata/blastdb/combined.fasta --efi-db smalldata/efi_db.sqlite --accessions-file smalldata/pf07476.txt
+python create_est_nextflow_params.py accessions --output-dir $OUTPUT_DIR --efi-config smalldata/efi.config --fasta-db smalldata/blastdb/combined.fasta --efi-db smalldata/efi_db.sqlite --accessions-file smalldata/pf07476.txt
 nextflow -C conf/docker.config run est.nf -params-file $OUTPUT_DIR/params.yml
 
 python create_ssn_nextflow_params.py auto --filter-min-val 87 --ssn-name testssn --ssn-title test-ssn --est-output-dir $OUTPUT_DIR
