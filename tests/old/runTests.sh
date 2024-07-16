769aa971ec5dc2ebe61bd0cf595a6909a81f3a41
Hunter DeMeyer <demeyer3@illinois.edu>
2024-07-16 14:30:50 -0500

    Delete unused test files

--- tests/old/runTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-perl module-Biocluster-Config.pl
-perl module-Biocluster-IdMapping-Builder-Database.pl
-perl module-Biocluster-IdMapping-Builder-Network.pl
-perl module-Biocluster-IdMapping-Builder-Other.pl
-perl module-Biocluster-IdMapping.pl
-
