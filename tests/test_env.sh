76c598dff9e46edb5018191dd8ebf8048c8658e0
Russell Davidson <rbdavid@illinois.edu>
2024-11-15 15:11:37 -0600

    add a results_dir env var and a help statement

--- tests/test_env.sh
+++ tests/test_env.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-# usage: source tests/test_env.sh [[--db-type mysql|sqlite]] [[--data-dir /path/to/data/directory]]
+# usage: source tests/test_env.sh [[--db-type mysql|sqlite]] [[--data-dir /path]] [[--results-dir /path]] [[--help]]
 # Optional inputs:
 #	--db-type, accepted values are mysql or sqlite
-#	--data-dir, a local or global path where the sample data will be untarred into
+#	--data-dir, a local or global path where the sample data will be 
+#		    untarred into
+#	--results-dir, a local or global path where the results from the test 
+#		       suite will be written
+#	--help, prints usage information
+
+# starting fresh
+EFI_DB_NAME=
+EFI_TEST_ENV=
+EFI_TEST_DATA_DIR=
+EFI_CONFIG_FILE=
+EFI_FASTA_DB=
+EFI_TEST_ACC_FILE=
+EFI_TEST_FASTA_FILE=
+EFI_TEST_BLAST_SEQ=
+EFI_TEST_FAMILY_ID=
+EFI_TEST_SSN_UNIPROT=
+EFI_TEST_SSN_UNIREF90=
+EFI_TEST_SSN_UNIREF50=
+EFI_TEST_SSN_REPNODE=
+EFI_TEST_RESULTS_DIR=
 
 # loop over input arguments
 for (( index=1; index <= "$#"; index++ ))
@@ -10,7 +30,20 @@ do
 	# get the next argument's index
 	idx=$((index+1))
 	# check if this argument matches a parameter string
-	if [[ ${!index} == "--db-type" ]]
+	if [[ ${!index} == '--help' ]]
+	then
+		echo "Usage information for test_env.sh:
+Must be in the EST root directory. 
+To run: source tests/test_env.sh [[--db-type mysql|sqlite]] [[--data-dir /path]] [[--results-dir /path]] [[--help]]
+Optional inputs:
+	--db-type, accepted values are mysql or sqlite, default: sqlite
+	--data-dir, a local or global path where the sample data will be 
+		    untarred into, default: tests/test_data
+	--results-dir, a local or global path where the results from the test 
+		       suite will be written, default: tests/test_results
+	--help, prints this usage information"
+		exit
+	elif [[ ${!index} == "--db-type" ]]
 	then
 		# grab the value of the next argument and save it in a var
 		db_type="${!idx}"
@@ -21,6 +54,11 @@ do
 		# grab the value of the next argument and save it in a var
 		data_dir="${!idx}"
 		echo "Testing input data will be untarred in $data_dir"
+	elif [[ ${!index} == "--results-dir" ]]
+	then
+		# grab the value of the next argument and save it in a var
+		results_dir="${!idx}"
+		echo "Testing results will be written in $results_dir"
 	fi
 done
 
@@ -33,6 +71,11 @@ fi
 if [[ -z $data_dir ]]
 then
 	data_dir="tests/test_data"
+fi
+
+if [[ -z $results_dir ]]
+then
+	results_dir="tests/test_results"
 fi
 
 # creating the necessary environment variables
@@ -57,4 +100,4 @@ export EFI_TEST_SSN_UNIPROT="$DATA_DIR/ssn.xgmml"
 export EFI_TEST_SSN_UNIREF90="$DATA_DIR/ssn_uniref90.xgmml"
 export EFI_TEST_SSN_UNIREF50="$DATA_DIR/ssn_uniref50.xgmml"
 export EFI_TEST_SSN_REPNODE="$DATA_DIR/ssn_repnode70.xgmml"
-
+export EFI_TEST_RESULTS_DIR=$results_dir
