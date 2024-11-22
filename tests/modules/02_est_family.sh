4a81534526e125b0b7cb0f34f9b0db59eaea4c30
Russell Davidson <rbdavid@illinois.edu>
2024-11-22 15:31:32 -0600

    remove redundant -with-dag calls and write dags to docs/ subdirs

--- tests/modules/02_est_family.sh
+++ tests/modules/02_est_family.sh
@@ -13,9 +13,9 @@ rm -rf $OUTPUT_DIR
 family=$(<$EFI_TEST_FAMILY_ID)
 
 ./bin/create_est_nextflow_params.py family --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --fasta-db $EFI_FASTA_DB --efi-db $EFI_DB_NAME --families $family --sequence-version uniprot
-nextflow -log $OUTPUT_DIR/est_nextflow.log -C $NXF_EST_CONFIG_FILE run pipelines/est/est.nf -params-file $OUTPUT_DIR/params.yml -w $OUTPUT_DIR/est_work -with-dag $OUTPUT_DIR/family_nf.html
+nextflow -log $OUTPUT_DIR/est_nextflow.log -C $NXF_EST_CONFIG_FILE run pipelines/est/est.nf -params-file $OUTPUT_DIR/params.yml -w $OUTPUT_DIR/est_work
 
 ./bin/create_generatessn_nextflow_params.py auto --filter-min-val 87 --ssn-name testssn --ssn-title test-ssn --est-output-dir $OUTPUT_DIR
 # ./bin/create_ssn_nextflow_params.py manual --filter-min-val 87 --ssn-name name --ssn-title title --blast-parquet smalldata/results/1.out.parquet --fasta-file smalldata/results/all_sequences.fasta --output-dir $OUTPUT_DIR/ssn --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME
-nextflow -log $OUTPUT_DIR/generatessn_nextflow.log -C $NXF_SSN_CONFIG_FILE run pipelines/generatessn/generatessn.nf -params-file $OUTPUT_DIR/ssn/params.yml -w $OUTPUT_DIR/generatessn_work -with-dag $OUTPUT_DIR/generatessn_nf.html
+nextflow -log $OUTPUT_DIR/generatessn_nextflow.log -C $NXF_SSN_CONFIG_FILE run pipelines/generatessn/generatessn.nf -params-file $OUTPUT_DIR/ssn/params.yml -w $OUTPUT_DIR/generatessn_work
 
