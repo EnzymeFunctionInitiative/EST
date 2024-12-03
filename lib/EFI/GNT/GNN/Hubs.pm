84910823d9429d1a53df1d77dfa5d2a869ce9e12
Nils Oberg <noberg@illinois.edu>
2024-12-03 16:30:44 -0600

    Refactor EFI::GNT::GNN::Hubs for code reuse, new attributes, and documentation

--- lib/EFI/GNT/GNN/Hubs.pm
+++ lib/EFI/GNT/GNN/Hubs.pm
@@ -4,6 +4,8 @@ package EFI::GNT::GNN::Hubs;
 use strict;
 use warnings;
 
+use List::Util qw(sum);
+
 
 sub new {
     my $class = shift;
@@ -31,40 +33,11 @@ sub new {
 }
 
 
-#TODO: documentation
+# public
 sub compute {
     my $self = shift;
     my $gnn = shift;
     $self->{cluster_data} = $gnn->getClusterData();
-
-    # internal structure looks like this:
-    #   cluster_pfam => {
-    #       "cluster_num" => {
-    #           pfams => {
-    #               "pfam_id" => {
-    #                   query_ids => [],
-    #                   genome_arrangement => [],
-    #                   distances => [],
-    #                   neighbors_query => [],
-    #                   neighbors => [],
-    #               },
-    #               "pfam_id2" => {
-    #                   ...
-    #               },
-    #               ...
-    #           },
-    #           # IDs in the cluster that have neighbors
-    #           hub_ids => [
-    #               "query",
-    #               "query",
-    #               ...
-    #           ] 
-    #       }
-    #   }
-    #
-    # The pfams associated with the cluster_num in this structure are Pfams
-    # from the neighbors not the query.
-    #
 
     foreach my $clusterNum (keys %{ $self->{cluster_data} }) {
         my $pfamData = {};
@@ -75,11 +48,11 @@ sub compute {
             my $attributes = $query->{attributes};
             my @neighborIds = @{ $query->{neighbors} };
             foreach my $nb (@neighborIds) {
-                push @{ $pfamData->{$nb->{pfam}}->{genome_arrangement} }, "$attributes->{id}:$attributes->{direction}:$nb->{id}:$nb->{direction}:$nb->{distance}";
-                push @{ $pfamData->{$nb->{pfam}}->{distances} }, $nb->{distance};
-                push @{ $pfamData->{$nb->{pfam}}->{neighbors_query} }, "$attributes->{id}:$nb->{id}";
-                push @{ $pfamData->{$nb->{pfam}}->{neighbors} }, $nb->{id};
-                $neighborPfamQuery->{$nb->{pfam}}->{$attributes->{id}} = 1;
+                push @{ $pfamData->{$nb->{pfam}}->{genome_arrangement} },   "$attributes->{id}:$attributes->{direction}:$nb->{id}:$nb->{direction}:$nb->{distance}"; #NOTE: old 'dist'
+                push @{ $pfamData->{$nb->{pfam}}->{distances} },            $nb->{distance}; #NOTE: old 'stats'
+                push @{ $pfamData->{$nb->{pfam}}->{neighbors_query} },      "$attributes->{id}:$nb->{id}"; #NOTE: old 'neigh'
+                push @{ $pfamData->{$nb->{pfam}}->{neighbors} },            $nb->{id}; #NOTE: old 'neighlist'
+                $neighborPfamQuery->{$nb->{pfam}}->{$attributes->{id}} = 1; #NOTE old 'orig'
             }
         }
     
@@ -87,13 +60,16 @@ sub compute {
         # the list of query IDs in the cluster that have neighbors
         my $hubIdsWithNeighbors = {};
         foreach my $pfam (keys %$neighborPfamQuery) {
-            my @ids = keys %{ $neighborPfamQuery->{$pfam} };
-            $pfamData->{$pfam}->{query_ids} = [sort @ids];
+            my @ids = sort keys %{ $neighborPfamQuery->{$pfam} };
+            #NOTE query_ids is equivalent to the 'orig' output from the old module, except this is
+            # unique whereas the old one wasn't
+            $pfamData->{$pfam}->{query_ids} = \@ids;
             # $neighborPfamQuery will only contain a Pfam/list of IDs if the query has neighbors
             map { $hubIdsWithNeighbors->{$_} = $pfam } @ids;
         }
 
         my @hubIdsWithNeighbors = [sort keys %$hubIdsWithNeighbors];
+        #NOTE hub_ids is equivalent to the 'withneighbors' output from the old module
         $self->{cluster_pfam}->{$clusterNum} = {pfam => $pfamData, hub_ids => \@hubIdsWithNeighbors};
     }
 
@@ -121,46 +97,73 @@ sub computePfamHubs {
         }
     }
 
-    # internal structure looks like this:
-    #   pfam_hubs => {
-    #       "pfam" => {
-    #           "cluster_num" => {
-    #               num_queries_with_neighbors => 0,
-    #               num_ids_in_pfam => 0,
-    #               cooccurrence => 0,
-    #           },
-    #           "cluster_num" => {
-    #               num_queries_with_neighbors => 0,
-    #               num_ids_in_pfam => 0,
-    #               cooccurrence => 0,
-    #           },
-    #       }
-    #   }
-
     foreach my $pfam (sort keys %pfams) {
         # Clusters that are associated with this Pfam
         my $clusters = {};
 
         foreach my $clusterNum (@{ $pfams{$pfam} }) {
-            my $numQueriesWithNeighbors = @{ $self->{cluster_pfam}->{$clusterNum}->{hub_ids} };
-            my $numIdsInPfam = @{ $self->{cluster_pfam}->{$clusterNum}->{pfam}->{$pfam}->{query_ids} };
-            my $numClusterIds = @{ $self->{cluster_data}->{$clusterNum} };
-            my $cooccurrence = int($numQueriesWithNeighbors / $numIdsInPfam * 100) / 100;
-
-            my $data = {
-                num_queries_with_neighbors => $numQueriesWithNeighbors,
-                num_ids_in_pfam => $numIdsInPfam,
-                num_cluster_ids => $numClusterIds,
-                cooccurrence => $cooccurrence,
-            };
-
+            my $pfamHub = $self->{cluster_pfam}->{$clusterNum}->{pfam}->{$pfam};
+            my $data = $self->makeHubData($clusterNum, $pfamHub);
             $clusters->{$clusterNum} = $data;
-            #TODO: threshold in output, [if ($numQueriesWithNeighbors > 1 and $cooccurrence >= $self->{cooc_threshold}) {]
+            #TODO: threshold in output, [if ($numIdsWithNeighbors > 1 and $cooccurrence >= $self->{cooc_threshold}) {]
         }
 
         $self->{pfam_hubs}->{$pfam} = $clusters;
     }
 
+}
+
+
+#
+# makeHubData - private method
+#
+# Creates a data structure for a cluster-Pfam hub combination.
+#
+# Parameters:
+#    $clusterNum - number of the cluster to use to create structure
+#    $pfamHub - the hub obtained from the cluster_pfam module variable
+#    $numIdsWithNeighbors - optional; used to reduce number of computations for cluster hubs
+#    $numClusterIds - optional; used to reduce number of computations for cluster hubs
+#
+# Returns:
+#    hash ref with keys corresponding to XGMML output attributes
+#
+sub makeHubData {
+    my $self = shift;
+    my $clusterNum = shift;
+    my $pfamHub = shift;
+    my $numIdsWithNeighbors = shift;
+    my $numClusterIds = shift;
+
+    $numIdsWithNeighbors = @{ $self->{cluster_pfam}->{$clusterNum}->{hub_ids} } if not defined $numIdsWithNeighbors; #NOTE old 'withneighbors'
+    $numClusterIds = @{ $self->{cluster_data}->{$clusterNum} } if not defined $numClusterIds;
+
+    my $numIdsInPfam = @{ $pfamHub->{query_ids} }; #NOTE old 'orig'
+    my $cooccurrence = int($numIdsInPfam / $numIdsWithNeighbors * 100) / 100;
+    my $coocRatio = "$numIdsInPfam/$numIdsWithNeighbors";
+    my $numNeighbors = @{ $pfamHub->{neighbors_query} }; #NOTE old 'neigh'
+
+    my $distanceSum = sum( @{ $pfamHub->{distances} } ); #NOTE old 'stats'
+    my $distanceMedian = median( @{ $pfamHub->{distances} } ); #NOTE old 'stats'
+    my $averageDist = int( $distanceSum / $numNeighbors * 100 ) / 100;
+    my $medianDist = int( $distanceMedian * 100 ) / 100;
+
+    my $data = {
+        num_ids_with_neighbors  => $numIdsWithNeighbors, #NOTE old 'withneighbors'
+        num_ids_in_pfam         => $numIdsInPfam, #NOTE old 'orig'
+        num_cluster_ids         => $numClusterIds,
+        cooccurrence            => $cooccurrence,
+        cooccurrence_ratio      => $coocRatio,
+        num_neighbors           => $numNeighbors,
+        arrangement             => $pfamHub->{genome_arrangement}, #NOTE old 'dist'
+        ids_in_pfam             => $pfamHub->{query_ids}, #NOTE old 'orig'
+        neighbors               => $pfamHub->{neighbors}, #NOTE old 'neighlist'
+        neighbors_query         => $pfamHub->{neighbors_query}, #NOTE old 'neigh'
+        average_distance        => sprintf("%.2f", $averageDist),
+        median_distance         => sprintf("%.2f", $medianDist),
+    };
+
+    return $data;
 }
 
 
@@ -175,44 +178,18 @@ sub computeClusterHubs {
 
     my @clusterNums = sort { $a <=> $b } keys %{ $self->{cluster_pfam} };
 
-    # internal structure that is stored looks like this:
-    #   cluster_hubs => {
-    #       "cluster_num" => {
-    #           "pfam" => {
-    #               ...
-    #            },
-    #           "pfam" => {
-    #               ...
-    #            },
-    #       },
-    #       "cluster_num" => {
-    #           "pfam" => {
-    #               ...
-    #            },
-    #           "pfam" => {
-    #               ...
-    #            },
-    #       },
-    #   }
-
     foreach my $clusterNum (@clusterNums) {
-        my $numQueriesWithNeighbors = @{ $self->{cluster_pfam}->{$clusterNum}->{hub_ids} };
+        my $numIdsWithNeighbors = @{ $self->{cluster_pfam}->{$clusterNum}->{hub_ids} };
         my $numClusterIds = @{ $self->{cluster_data}->{$clusterNum} };
 
-        next if $numQueriesWithNeighbors < 2;
+        next if $numIdsWithNeighbors < 2;
 
         # Pfams that are associated with this cluster
         my $pfams = {};
 
         foreach my $pfam (sort keys %{ $self->{cluster_pfam}->{$clusterNum}->{pfam} }) {
-            my $numIdsInPfam = @{ $self->{cluster_pfam}->{$clusterNum}->{pfam}->{$pfam}->{query_ids} };
-            my $cooccurrence = int($numIdsInPfam / $numQueriesWithNeighbors * 100) / 100;
-            my $data = {
-                num_queries_with_neighbors => $numQueriesWithNeighbors,
-                num_ids_in_pfam => $numIdsInPfam,
-                num_cluster_ids => $numClusterIds,
-                cooccurrence => $cooccurrence,
-            };
+            my $pfamHub = $self->{cluster_pfam}->{$clusterNum}->{pfam}->{$pfam};
+            my $data = $self->makeHubData($clusterNum, $pfamHub, $numIdsWithNeighbors, $numClusterIds);
             $pfams->{$pfam} = $data;
             #TODO: threshold in output if ($cooccurrence > $self->{cooc_threshold}) {
         }
@@ -222,12 +199,37 @@ sub computeClusterHubs {
 }
 
 
+#
+# median - private function
+#
+# Computes the median of the input list.
+#
+# Parameters:
+#    @vals - list of numeric values
+#
+# Returns:
+#    median value of the input list, a numeric value
+#
+sub median {
+    my @vals = sort {$a <=> $b} @_;
+    my $len = @vals;
+
+    if ($len % 2) { # returns non-zero value if it is not even
+        return $vals[int($len / 2)];
+    } else {
+        return ($vals[int($len / 2) - 1] + $vals[int($len / 2)]) / 2;
+    }
+}
+
+
+# public
 sub getClusterHubNumbers {
     my $self = shift;
     return sort { $a <=> $b } keys %{ $self->{cluster_hubs} };
 }
 
 
+# public
 sub getClusterHub {
     my $self = shift;
     my $clusterNum = shift;
@@ -239,12 +241,14 @@ sub getClusterHub {
 }
 
 
+# public
 sub getPfamHubNames {
     my $self = shift;
     return sort keys %{ $self->{pfam_hubs} };
 }
 
 
+# public
 sub getPfamHub {
     my $self = shift;
     my $pfam = shift;
@@ -257,6 +261,70 @@ __END__
 
 =pod
 
+=head1 EFI::GNT::GNN::Hubs
+
+=head2 NAME
+
+B<EFI::GNT::GNN::Hubs> - Perl helper module for computing Pfam and cluster GNN data
+
+=head2 SYNOPSIS
+
+    my $cooccurrenceThreshold = 0.20;
+    my $gnn = new EFI::GNT::GNN(...);
+    my $hubs = new EFI::GNT::GNN::Hubs(gnn => $gnn, cooc_threshold => $cooccurrenceThreshold);
+    $hubs->compute();
+
+    my $clusterNums = $hubs->getClusterHubNumbers();
+    my $cluster1Hub = $hubs->getClusterHub($clusterNums[0]);
+    
+    my $pfamHubNames = $hubs->getPfamHubNames();
+    my $pfamHub = $hubs->getPfamHub($pfamHubNames[0]);
+
+
+=head2 DESCRIPTION
+
+B<EFI::GNT::GNN::Hubs> is a Perl helper module for computing hub data for the Pfam and
+cluster hubs.  Data can be retrieved after computation and used by modules such as
+B<EFI::GNT::GNN::XgmmlWriter::PfamHub> and B<EFI::GNT::GNN::XgmmlWriter::ClusterHub>.
+
+
+=head2 METHODS
+
+=head3 C<new(gnn => $gnn, cooc_threshold => $value)>
+
+Creates an object.
+
+=head4 Parameters
+
+=item
+
+=over C<gnn>
+
+A B<EFI::GNT::GNN> object.
+
+=over C<cooc_threshold>
+
+The cooccurrence threshold, used to determine if a cluster hub or Pfam hub should be
+included in the output network.  A numerical value > 0 and <= 1.  If not specified,
+defaults to C<0.20>.
+
+=back
+
+=head4 Example Usage
+
+    my $cooccurrenceThreshold = 0.20;
+    my $hubs = new EFI::GNT::GNN::Hubs(gnn => $gnn, cooc_threshold => $cooccurrenceThreshold);
+
+
+=head3 C<compute()>
+
+Computes hub connectivity for the Pfam and Cluster hub GNNs.  The data used
+for computing is obtained from the C<gnn> parameter passed to the constructor.
+
+=head4 Example Usage
+
+    $hubs->compute();
+
 
 =head3 C<getClusterHubNumbers()>
 
@@ -265,7 +333,7 @@ the same as the cluster numbers in the input network.
 
 =head4 Returns
 
-An array of numerical cluster numbers
+An array of numerical cluster numbers.
 
 =head4 Example Usage
 
@@ -277,7 +345,7 @@ An array of numerical cluster numbers
 
 =head3 C<getClusterHub($clusterNum, $allClusters)>
 
-Returns cluster hub data for a given cluster number
+Returns cluster hub data for a given cluster number.
 
 =head4 Parameters
 
@@ -285,18 +353,18 @@ Returns cluster hub data for a given cluster number
 
 =item C<$clusterNum>
 
-Numerical cluster number
+Numerical cluster number.
 
 =item C<$allClusters>
 
 If specified and non-zero, then all clusters are returned, even those not meeting
-the cooccurrence threshold.  Defaults to 0
+the cooccurrence threshold.  Defaults to 0.
 
 =back
 
 =head4 Returns
 
-A hash ref pointing to an array of hash refs, each with data about a cluster hub
+A hash ref pointing to an array of hash refs, each with data about a cluster hub.
 
     [
         {}
@@ -318,11 +386,11 @@ A hash ref pointing to an array of hash refs, each with data about a cluster hub
 
 =head3 C<getPfamHubNames()>
 
-Returns the list of Pfam hub names that are in the GNN
+Returns the list of Pfam hub names that are in the GNN.
 
 =head4 Returns
 
-An array of Pfam hub IDs (can be dash-separated)
+An array of Pfam hub IDs (can be dash-separated).
 
 =head4 Example Usage
 
@@ -334,7 +402,7 @@ An array of Pfam hub IDs (can be dash-separated)
 
 =head3 C<getPfamHub($pfamName)>
 
-Returns Pfam hub data for a given Pfam hub name
+Returns Pfam hub data for a given Pfam hub name.
 
 =head4 Parameters
 
@@ -342,7 +410,7 @@ Returns Pfam hub data for a given Pfam hub name
 
 =item C<$pfamName>
 
-Pfam hub name that is in the GNN
+Pfam hub name that is in the GNN.
 
 =back
 
