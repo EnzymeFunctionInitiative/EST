09b15b9c35ee6cb91e1a7c8c85dbfc0c75029929
Nils Oberg <noberg@illinois.edu>
2024-12-03 16:28:21 -0600

    Refactor EFI::GNT::Annotations for clarity and code reuse, and document

--- lib/EFI/GNT/Annotations.pm
+++ lib/EFI/GNT/Annotations.pm
@@ -18,42 +18,43 @@ sub new {
     bless($self, $class);
 
     $self->{dbh} = $args{dbh};
-    $self->{anno} = new EFI::Annotations;
-    $self->{accession_pdb} = {};
-    $self->{hub_pdb_info} = {};
+    $self->{efi_anno} = new EFI::Annotations;
+    $self->{anno_cache} = {};
+    $self->{anno_sql} = "SELECT taxonomy_id, swissprot_status, metadata FROM annotations WHERE accession = ?";
 
     return $self;
 }
 
 
 # public
-sub getAnnotations {
+sub getGnnIdAnnotations {
     my $self = shift;
     my $accessionData = shift;
 
-    my $accession = $accessionData->{id};
-
-    my ($orgs, $taxIds, $status, $descs) = $self->getMultipleAnnotations($accession);
-
-    my $organism = $orgs->{$accession};
-    my $taxId = $taxIds->{$accession};
-    my $annoStatus = $status->{$accession};
-    my $desc = $descs->{$accession};
+    my $data = $self->retrieveIdAnnotations($accessionData->{id});
 
     my $pfamDesc = "";
     if ($accessionData->{pfam}) {
         my $names = $self->getFamilyNames($accessionData->{pfam});
-        $pfamDesc = join(";", map { $_->{short} } grep { $_->{family} =~ m/^PF/ } @$names);
+        $pfamDesc = join(";", map { $_->{short} } grep { $_->{id} =~ m/^PF/ } @$names);
     }
 
     my $interproDesc = "";
-    my @interproFamilies = map { $_->{family} } @{ $accessionData->{interpro} };
-    if (@interproFamilies) {
-        my $names = $self->getFamilyNames(@interproFamilies);
-        $interproDesc = join(";", map { $_->{short} } grep { $_->{family} =~ m/^IPR/ } @$names);
+    if ($accessionData->{interpro}) {
+        my $names = $self->getFamilyNames($accessionData->{interpro});
+        $interproDesc = join(";", map { $_->{short} } grep { $_->{id} =~ m/^IPR/ } @$names);
     }
 
-    return {organism => $organism, taxonomy_id => $taxId, status => $annoStatus, desc => $desc, pfam_desc => $pfamDesc, interpro_desc => $interproDesc};
+    my $annoData = {
+        organism => $data->{organism},
+        taxonomy_id => $data->{taxonomy_id},
+        status => $data->{is_swissprot},
+        desc => $data->{desc},
+        pfam_desc => $pfamDesc,
+        interpro_desc => $interproDesc,
+    };
+
+    return $annoData;
 }
 
 
@@ -78,149 +79,134 @@ sub getFamilyNames {
         push @names, {family => $family, short => $short, long => $long};
     }
 
-    return \@names;
+    my $allLong = join("-", map { $_->{long} } @names);
+    my $allShort = join("-", map { $_->{short} } @names);
+
+    return \@names, $allShort, $allLong;
 }
 
 
 #
-# getMultipleAnnotations - internal method
+# retrieveIdAnnotations - internal method
 #
-# Returns annotations for one or more sequence IDs.
+# Retrieves annotation data from the internal cache if the annotations have
+# already been retrieved, or does a database lookup otherwise.
 #
 # Parameters:
-#    $accessions - scalar: one accession ID; hash ref, return results for
-#       each accession ID
+#    $id - EFI database accession ID
+#    $sth - optional; DBI statement handle, used to improve bulk retrievals
 #
 # Returns:
-#    hash ref of organism(s)
-#    hash ref of taxonomy ID(s)
-#    hash ref of annotation status(s)
-#    hash ref of SwissProt description(s)
+#    a hash ref containing is_swissprot, pdb, ec, organism, taxonomy_id, and
+#        desc (SwissProt description) fields
 #
-sub getMultipleAnnotations {
+sub retrieveIdAnnotations {
     my $self = shift;
-    my $accessions = shift;
+    my $id = shift;
+    my $sth = shift;
 
-    # If it's a single scalar accession convert it to an arrayref.
-    if (ref $accessions ne "ARRAY") {
-        $accessions = [$accessions];
+    if ($self->{anno_cache}->{$id}) {
+        return $self->{anno_cache}->{$id};
     }
 
-    my (%organism, %taxId, %annoStatus, %desc);
-
-    my $spCol = "swissprot_status";
-    my $orgCol = "organism";
-    my $taxCol = "taxonomy_id";
-    my $descCol = "description";
-    my $baseSql = "SELECT $taxCol, $spCol, metadata FROM annotations";
-
-    foreach my $accession (@$accessions) {
-        my $sql = "$baseSql where accession='$accession'";
-    
-        my $sth = $self->{dbh}->prepare($sql);
-        $sth->execute;
-    
-        if (not $self->{dbh}->ping()) {
-            warn "Database disconnected at " . scalar localtime;
-            $self->{dbh} = $self->{dbh}->clone() or die "Cannot reconnect to database.";
-        }
-
-        if (my $row = $sth->fetchrow_hashref) {
-            #TODO: put error somewhere
-            print "WARNING: missing metadata for $accession; is entry obsolete? [2]\n" if not $row->{metadata};
-            my $struct = $self->{anno}->decode_meta_struct($row->{metadata});
-            $organism{$accession} = $struct->{$orgCol};
-            $desc{$accession} = $struct->{$descCol};
-            $taxId{$accession} = $row->{$taxCol};
-            $annoStatus{$accession} = $row->{$spCol};
-        }
+    if (not $sth) {
+        $sth = $self->{dbh}->prepare($self->{anno_sql});
+        $sth->execute($id);
     }
 
-    return (\%organism, \%taxId, \%annoStatus, \%desc);
-}
-
-
-sub initPdbInfo {
-    my $self = shift;
-    my $source = shift;
-
-    my $clusters = $source->getClusters();
-
-    foreach my $clusterNum (@$clusters) {
-        #TODO: compute pdb info on a pfam hub basis
-        my $pfamHubIds = $source->getSequenceIds($clusterNum);
-        foreach my $pfamHub (keys %$pfamHubIds) {
-            my ($shape, $pdbInfo) = $self->retrieveHubPdbInfo($pfamHubIds->{$pfamHub});
-            $self->{hub_pdb_info}->{$clusterNum}->{$pfamHub} = [$shape, $pdbInfo];
-        }
+    my $row = $sth->fetchrow_hashref();
+    if (not $row) {
+        $self->addWarning("WARNING: $id missing from EFI database; is entry obsolete?");
+        return undef;
     }
+
+    my $metadata = {};
+    if ($row->{metadata}) {
+        $metadata = $self->{efi_anno}->decode_meta_struct($row->{metadata});
+    } else {
+        $self->addWarning("WARNING: missing metadata for $id; is entry obsolete?");
+    }
+
+    my $data = {
+        is_swissprot => $row->{swissprot_status},
+        pdb => $metadata->{pdb} // "",
+        ec => $metadata->{ec_code} // "",
+        organism => $metadata->{organism} // "",
+        taxonomy_id => $metadata->{taxonomy_id} // 0,
+        desc => $metadata->{description} // "",
+    };
+
+    $self->{anno_cache}->{$id} = $data;
+
+    return $data;
 }
 
 
 # public
-sub getHubPdbShape {
+sub getHubAnnotations {
     my $self = shift;
-    my $clusterNum = shift;
-    my $pfamHub = shift;
-    return $self->{hub_pdb_info}->{$clusterNum}->{$pfamHub}->[0];
+    my $ids = shift;
+
+    my $numPdb = 0;
+    my $numSwissProt = 0;
+
+    # Prepare and cache the database statement handle
+    my $sth = $self->{dbh}->prepare($self->{anno_sql});
+
+    my $hubInfo = {};
+    foreach my $accession (@$ids) {
+        my $data = $self->retrieveIdAnnotations($accession, $sth);
+
+        my $pdbEvalue = "None";
+        my $closestPdbNumber = "None";
+        my $status = $data->{is_swissprot} ? "SwissProt" : "TrEMBL";
+
+        my $info = join(":", $data->{ec}, $data->{pdb}, $closestPdbNumber, $pdbEvalue, $status);
+
+        $hubInfo->{$accession} = $info;
+
+        $numSwissProt++ if $data->{is_swissprot};
+        $numPdb++ if $data->{pdb};
+    }
+
+    return $hubInfo, $numPdb, $numSwissProt;
 }
 
 
-# private
-sub retrieveHubPdbInfo {
+# public
+sub getHubShape {
     my $self = shift;
-    my $accessions = shift;
-
-    #                    foreach my $neighbor (@{ $clusterData->{$clusterId}->{$pfamHub}->{neighbors} }) {
-    #                        my ($shape, $pdbinfo) = $self->getPdbInfo($clusterData->{$clusterId}->{$pfamHub}->{neighbors});
-    #    (my $shape, my $pdbinfo)= $self->getPdbInfo(\@{${$clusterData}{$clusterId}{$pfam}{'neighlist'}});
-    #                        my $pdb = "$clusterNum:$neighbor:" . $pdbInfo->{$neighbor};
-    #                        push @hubPdb, $pdb;
-    #                    }
-
-    my @accessions = @$accessions;
-
-    my $pdbValueCount = 0;
-    my $reviewedCount = 0;
-
-    my $sql = "SELECT swissprot_status, metadata FROM annotations WHERE accession = ?";
-    my $sth = $self->{dbh}->prepare($sql);
-
-    foreach my $accession (@accessions) {
-        my $info = $self->getPdbForAccession($accession);
-        if (not $info) {
-            $info = $self->retrievePdbData($accession, $sth);
-        }
-
-        $reviewedCount++ if $info->{status};
-        $pdbValueCount++ if $info->{pdb_num} ne "None";
-    }
+    my $numPdb = shift;
+    my $numSwissProt = shift;
 
     my $shape = "circle";
-    if ($pdbValueCount > 0 and $reviewedCount > 0) {
+    if ($numPdb > 0 and $numSwissProt > 0) {
         $shape = "diamond";
-    } elsif ($pdbValueCount > 0) {
+    } elsif ($numPdb > 0) {
         $shape = "square";
-    } elsif ($reviewedCount > 0) {
+    } elsif ($numSwissProt > 0) {
         $shape = "triangle"
     }
 
-    #TODO:
-    my $pdbInfo = {};
-    return $shape, $pdbInfo;
+    return $shape;
 }
 
 
-# public
-sub getPdbForAccession {
-    my $self = shift;
-    my $accession = shift;
-    return $self->{accession_pdb}->{$accession};
-}
-
-
-# private
-sub retrievePdbData {
+#
+# getHubIdAnnotations - private method
+#
+# Retrieves SwissProt, PDB, and EC number for a given accession.  The
+# results are cached.
+#
+# Parameters:
+#    $accession - EFI database accession ID (typically for neighbors)
+#    $sth - database (DBI) handle; cached for performance
+#
+# Returns:
+#    hash ref structure with keys for SwissProt status, PDB number, and
+#        those annotations joined together into an info string
+#
+sub getHubIdAnnotations {
     my $self = shift;
     my $accession = shift;
     my $sth = shift;
@@ -246,8 +232,6 @@ sub retrievePdbData {
     $info->{pdb_num} = $pdbNumber;
     $info->{all} = join(":", $ecNum, $pdbNumber, $closestPdbNumber, $pdbEvalue, $status);
 
-    $self->{accession_pdb}->{$accession} = $info;
-
     return $info;
 }
 
@@ -268,10 +252,13 @@ EFI::GNT::Annotations - Perl module for retrieving annotations from the EFI data
     use EFI::GNT::Annotations;
 
     my $annoUtil = new EFI::GNT::Annotations(dbh => $dbh);
-    my $id = "B0SS77";
-    my $pfams = "PF05544-PF05555";
-    my $interpros = "IPR007197";
-    my $annoData = $annoUtil->getAnnotations($id, $pfams, $interpros);
+    my $idData = {id => "B0SS77", pfam => "PF07478-PF01820", interpro => "IPR011761-IPR013815-IPR005905-IPR011095-IPR011127-IPR016185"};
+    my $annoData = $annoUtil->getGnnIdAnnotations($idData);
+
+    my $neighbors = ["B0SS77", "B0SS79"];
+    my ($neighborData, $numPdb, $numSwissProt) = $annoUtil->getHubAnnotations($neighbors);
+
+    my $shape = $annoUtil->getHubShape($numPdb, $numSwissProt);
 
 
 =head2 DESCRIPTION
@@ -301,25 +288,24 @@ Database handle that comes from C<EFI::Database>.
     my $annoUtil = new EFI::GNT::Annotations(dbh => $dbh);
 
 
-=head3 C<getAnnotations($id, $pfamFamilies, $interproFamilies)>
+=head3 C<getGnnIdAnnotations($idData)>
 
-Retrieves the annotations for the sequence ID C<$id>.
+Retrieves annotations for the accession ID that are necessary to create a GNN.
 
 =head4 Parameters
 
 =over
 
-=item C<$id>
+=item C<$idData>
 
-Sequence ID to retrieve metadata for.
+A hash ref containing a EFI database accession ID and the associated Pfam and
+InterPro family IDs (multiple can be specified, hyphen-separated).  For example,
 
-=item C<$pfamFamilies>
-
-Hyphen-separated list of Pfam families associated with the sequence.
-
-=item C<$interproFamilies>
-
-Hyphen-separated list of InterPro families associated with the sequence.
+    {
+        id => "B0SS77",
+        pfam => "PF07478-PF01820",
+        interpro => "IPR011761-IPR013815-IPR005905-IPR011095-IPR011127-IPR016185"}
+    }
 
 =back
 
@@ -338,19 +324,147 @@ A hash ref with the keys pointing to metadata values:
 
         desc => "SwissProt description",
 
-        # description for each Pfam family
-        pfam_desc => "Pfam descriptions",
+        # description for each input Pfam family ID
+        pfam_desc => "Dala_Dala_lig_C;Dala_Dala_lig_N",
 
-        # description for each InterPro family
-        interpro_desc => "InterPro descriptions"
+        # description for each input InterPro family ID
+        interpro_desc => "ATP-grasp;ATP_grasp_subdomain_1;D_ala_D_ala;Dala_Dala_lig_C;Dala_Dala_lig_N;PreATP-grasp_dom_sf"
     }
 
 =head4 Example Usage
 
-    my $annoData = $annoUtil->getAnnotations($id, $pfamFamilies, $interproFamilies);
+    my $annoData = $annoUtil->getGnnIdAnnotations($id);
     foreach my $annoKey (keys %$annoData) {
         print "$annoKey: $annoData->{$annoKey}\n";
     }
+
+
+=head3 C<getFamilyNames($familyHubName)>
+
+Retrieves the family names for the families specified in the hub name.  A family hub
+is one or more Pfam or InterPro family IDs separated by a hyphen (C<->).
+
+=head4 Parameters
+
+=over
+
+=item C<$familyHubName>
+
+Hyphen-separated family IDs (for example, C<PF05544-PF07197>).
+
+=back
+
+=head4 Returns
+
+Returns three parameters:
+
+=over
+
+=item C<$names>
+
+An array ref where each element is a hash ref that has three keys (C<ID>, C<short>,
+and C<long>).  C<short> is the family short name, and C<long> is the family long name.
+For example:
+
+    [
+        {id => "PF05544", short => "Pro_racemase", long => "Proline racemase"},
+        {id => "PF07197", short => "DUF1409", long => "Protein of unknown function (DUF1409)"}
+    ]
+
+=item C<$allShort>
+
+A string with all of the input family short names joined with hyphens, for example
+C<"Pro_racemase-DUF1409">.
+
+=item C<$allLong>
+
+A string with all of the input family long names joined with hyphens, for example
+C<"Proline racemase-Protein of unknown function (DUF1409)">.
+
+=back
+
+=head4 Example Usage
+
+    my $pfamHub = "PF05544-PF07197";
+    my ($nameInfo, $allShort, $allLong) = $gntAnno->getFamilyNames($pfamHub);
+    foreach my $info (@$nameInfo) {
+        print "Family ID: $info->{id}, Short name: $info->{short}, Long name: $info->{long}\n";
+    }
+
+    my $pfamDesc = join("; ", map { $_->{long} } grep { $_->{id} =~ m/^PF/ } @$nameInfo);
+
+
+=head3 C<getHubAnnotations($ids)>
+
+Retrieves PDB and EC number information for all of the input IDs (usually the
+Pfam hub neighbors).
+
+=head4 Parameters
+
+=over
+
+=item C<$ids>
+
+An array ref where each element is an EFI database accession ID, typically hub neighbors.
+
+=back
+
+=head4 Returns
+
+=over
+
+=item C<$hubData>
+
+A hash ref that maps input IDs to a string containing PDB/EC/SwissProt information.
+
+=item C<$numPdb>
+
+The number of IDs in the input list that have a PDB number.
+
+=item C<$numSwissProt>
+
+The number of IDs in the input list that have SwissProt annotations.
+
+=back
+
+=head4 Example Usage
+
+    my $nb = ["B0SS77"];
+    my ($data, $numPdb, $numSwissProt) = $annoUtil->getHubAnnotations($nb);
+
+
+=head3 C<getShape($numPdb, $numSwissProt)>
+
+Returns the shape that the node in the GNN (XGMML) file will have, based on the number
+of neighbors in the hub that have PDB numbers and SwissProt annotations.
+
+=head4 Parameters
+
+=over
+
+=item C<$numPdb>
+
+The number of IDs in the input list that have a PDB number.
+
+=item C<$numSwissProt>
+
+The number of IDs in the input list that have SwissProt annotations.
+
+=back
+
+=head4 Returns
+
+The shape (as a string) that will be associated with the node in the output GNN.
+Available types are: C<diamond> (has PDB and SwissProt), C<square> (has PDB but
+no SwissProt), C<triangle> (has SwissProt but no PDB), and C<circle> (no PDB nor
+SwissProt).
+
+=head4 Example Usage
+
+    my ($data, $numPdb, $numSwissProt) = $annoUtil->getHubAnnotations([...]);
+    my $shape = $annoUtil->getHubShape($numPdb, $numSwissProt);
+    print "The Hub shape will be $shape\n";
+
 
 =cut
 
