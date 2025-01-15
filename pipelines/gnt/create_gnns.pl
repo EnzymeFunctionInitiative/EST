fd13aba0e3b2064e60e883b1b8f8d6da4f4ab6a3
Nils Oberg <noberg@illinois.edu>
2025-01-15 15:55:36 -0600

    Add detail to documentation

--- pipelines/gnt/create_gnns.pl
+++ pipelines/gnt/create_gnns.pl
@@ -128,60 +128,72 @@ neighborhood diagram (GND) file.
 
 =item C<--cluster-map>
 
-Path to a file that maps UniProt sequence ID to a cluster number, which can include
-a list of singletons.
+Path to the input file that maps UniProt sequence ID to a cluster number, which
+can include a list of singletons (i.e. no cluster number columns).  See
+C<parse_cluster_map_file()> in B<EFI::SSN::Util::ID> for an explanation of the
+file format.
 
 =item C<--cluster-gnn>
 
-Path to the output cluster-centric GNN in XGMML (XML) format.
+Path to the output cluster-centric GNN in XGMML (XML) format.  This file can be
+viewed in Cytoscape.
 
 =item C<--pfam-gnn>
 
-Path to the output Pfam-centric GNN in XGMML (XML) format.
+Path to the output Pfam-centric GNN in XGMML (XML) format.  This file can be
+viewed in Cytoscape.
 
 =item C<--gnd>
 
-Optional path to an output file in SQLite format that contains the data necessary
+Optional path to an output file in SQLite format containing the data necessary
 to visualize genome neighborhood diagrams (GNDs).
 
 =item C<--cooc-table>
 
-Path to a file that will contain co-occurrences for every Pfam of every neighbor of
-every ID in the input ID list.
+Optional path to an output file containing co-occurrences for every Pfam of
+every neighbor of every ID in the input ID list.  The file is a tab-separated
+file with the first column being a list of Pfams and each successive column
+being a cluster number and the co-occurrence of the Pfam in that cluster.
 
 =item C<--hub-count>
 
-Path to a file that will contain the size of every cluster hub (e.g. how many
-IDs had neighbors vs how how many IDs in the cluster).
+Optional path to an output tab-separated file containing the size of every
+cluster hub, with the first column being the cluster number, the second column
+(NumQueryableSeq) containing the number of sequences in the cluster that had
+neighbors, and the third column (TotalNumSeq) containing the total number of
+sequences in the cluster.
 
 =item C<--nb-pfam-list-dir>
 
-Path to a directory that will contain tables for every Pfam for all of the
-neighbors of the input IDs. Four sub-directories are created: C<pfam>,
-C<pfam_split>, C<all_pfam>, and C<all_pfam_split>.
+Optional path to an output directory containing tables for every Pfam group
+for all of the neighbors of the input IDs.  Four sub-directories are created:
+C<pfam> (Pfam groups filtered by co-occurrence), C<pfam_split> (Pfam groups
+split into constituent families, filtered by co-occurrence), C<all_pfam> (all
+Pfam groups, not filtered by co-occurrence), and C<all_pfam_split> (Pfam
+groups split into constituent families, not filtered by co-occurrence).
 
 =item C<--no-context>
 
-Path to a file that will contain a list of input IDs without ENA data or
-without neighbors.
+Optional path to an output file that contains a list of input IDs without
+ENA data or without neighbors.
 
 =item C<--nb-size>
 
-Number of neighbors on the left and right of the input IDs to include in the
-analysis. This is an integer > 0.
+Optional number of neighbors on the left and right of the input IDs to
+include in the analysis, an integer > 0.
 
 =item C<--cooc-threshold>
 
-The co-occurrence threshold to use for computing the Pfam hubs. This is a
-real number >= 0 and <= 1.
+Optional co-occurrence threshold to use for computing the Pfam hubs, a real
+number >= 0 and <= 1.
 
 =item C<--config>
 
-Path to the C<efi.config> file used for database connection options
+Path to the C<efi.config> file used for database connection options.
 
 =item C<--db-name>
 
-Name of the database to use (path to file for SQLite)
+Name of the database to use (path to file for SQLite).
 
 =back
 
