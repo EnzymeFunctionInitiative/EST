2a68fabf2ba9f6fe179501705aeeb8ec0c3fc576
Nils Oberg <noberg@illinois.edu>
2025-01-22 15:01:48 -0600

    Documentation updates
    - Add documentation for GND pipeline and modules
    - Update GNT documentation to reflect changes to script

--- docs/source/pipelines/gnt/create_gnns.rst
+++ docs/source/pipelines/gnt/create_gnns.rst
@@ -39,46 +39,61 @@ Arguments
 ~~~~~~~~~
 
 ``--cluster-map``
-   Path to a file that maps UniProt sequence ID to a cluster number,
-   which can include a list of singletons.
+   Path to the input file that maps UniProt sequence ID to a cluster
+   number, which can include a list of singletons (i.e. no cluster
+   number columns). See ``parse_cluster_map_file()`` in
+   **EFI::SSN::Util::ID** for an explanation of the file format.
 
 ``--cluster-gnn``
-   Path to the output cluster-centric GNN in XGMML (XML) format.
+   Path to the output cluster-centric GNN in XGMML (XML) format. This
+   file can be viewed in Cytoscape.
 
 ``--pfam-gnn``
-   Path to the output Pfam-centric GNN in XGMML (XML) format.
+   Path to the output Pfam-centric GNN in XGMML (XML) format. This file
+   can be viewed in Cytoscape.
 
 ``--gnd``
-   Optional path to an output file in SQLite format that contains the
-   data necessary to visualize genome neighborhood diagrams (GNDs).
+   Optional path to an output file in SQLite format containing the data
+   necessary to visualize genome neighborhood diagrams (GNDs).
 
 ``--cooc-table``
-   Path to a file that will contain co-occurrences for every Pfam of
-   every neighbor of every ID in the input ID list.
+   Optional path to an output file containing co-occurrences for every
+   Pfam of every neighbor of every ID in the input ID list. The file is
+   a tab-separated file with the first column being a list of Pfams and
+   each successive column being a cluster number and the co-occurrence
+   of the Pfam in that cluster.
 
 ``--hub-count``
-   Path to a file that will contain the size of every cluster hub (e.g.
-   how many IDs had neighbors vs how how many IDs in the cluster).
+   Optional path to an output tab-separated file containing the size of
+   every cluster hub, with the first column being the cluster number,
+   the second column (NumQueryableSeq) containing the number of
+   sequences in the cluster that had neighbors, and the third column
+   (TotalNumSeq) containing the total number of sequences in the
+   cluster.
 
 ``--nb-pfam-list-dir``
-   Path to a directory that will contain tables for every Pfam for all
-   of the neighbors of the input IDs. Four sub-directories are created:
-   ``pfam``, ``pfam_split``, ``all_pfam``, and ``all_pfam_split``.
+   Optional path to an output directory containing tables for every Pfam
+   group for all of the neighbors of the input IDs. Four sub-directories
+   are created: ``pfam`` (Pfam groups filtered by co-occurrence),
+   ``pfam_split`` (Pfam groups split into constituent families, filtered
+   by co-occurrence), ``all_pfam`` (all Pfam groups, not filtered by
+   co-occurrence), and ``all_pfam_split`` (Pfam groups split into
+   constituent families, not filtered by co-occurrence).
 
 ``--no-context``
-   Path to a file that will contain a list of input IDs without ENA data
-   or without neighbors.
+   Optional path to an output file that contains a list of input IDs
+   without ENA data or without neighbors.
 
 ``--nb-size``
-   Number of neighbors on the left and right of the input IDs to include
-   in the analysis. This is an integer > 0.
+   Optional number of neighbors on the left and right of the input IDs
+   to include in the analysis, an integer > 0.
 
 ``--cooc-threshold``
-   The co-occurrence threshold to use for computing the Pfam hubs. This
-   is a real number >= 0 and <= 1.
+   Optional co-occurrence threshold to use for computing the Pfam hubs,
+   a real number >= 0 and <= 1.
 
 ``--config``
-   Path to the ``efi.config`` file used for database connection options
+   Path to the ``efi.config`` file used for database connection options.
 
 ``--db-name``
-   Name of the database to use (path to file for SQLite)
+   Name of the database to use (path to file for SQLite).
--- lib/EFI/GNT/GND.pm
+++ lib/EFI/GNT/GND.pm
@@ -454,7 +454,114 @@ as position on the genome, taxonomic identifier, family data, plus more.  The
 structure is serialized into two tables, the C<attribute> table with one row for
 every ID in the cluster and the C<neighbors> table for the neighbors of each
 query.  The C<neighbors> table is linked to the C<query> table through the use
-of the C<query_sort_key> field which maps to the query C<sort_key> field.
+of the C<gene_key> field which maps to the query C<sort_key> field.  The schema
+is defined as follows:
+
+    Table attributes {
+        // Primary key, unique to this table
+        sort_key integer [primary key]
+        // UniProt ID
+        accession varchar(20)
+        // ENA genome ID
+        embl_id varchar(30)
+        // The number on the genome
+        num integer
+        // Pfam family ID(s)
+        family text
+        // InterPro family ID(s)
+        ipro_family text
+        // Start codon of the AA sequence on the genome
+        start integer
+        // End codon of the AA sequence on the genome
+        stop integer
+        // Start codon, but relative to the start of this sequence; for values
+        // in this table this will always be zero
+        rel_start integer
+        // End codon, but relative to the start of this sequence; for values
+        // in this table this will always be the sequence length
+        rel_stop integer
+        // Direction of the sequence, either 'normal' or 'complement'
+        direction varchar(10)
+        // Type of the sequence, either 'linear' or 'circular'
+        type varchar(8)
+        // Length of the sequence
+        seq_len integer
+        // Taxonomy identifier as provided by NCBI
+        taxon_id integer
+        // SwissProt status; 1 if the sequence is a SwissProt sequence, 0 if TrEMBL
+        anno_status integer
+        // Sequence description if SwissProt
+        desc text
+        // Pfam family description(s)
+        family_desc text
+        // InterPro family description(s)
+        ipro_family_desc text
+        // Sequence color, based on Pfam
+        color varchar(255)
+        // Sorting order in the display
+        sort_order integer
+        // Organism strain
+        strain text
+        // The number in the cluster; 0 if there is no cluster associated
+        cluster_num integer
+        // The organism that this sequence belongs to
+        organism text
+        // This will be 1 if the neighbor exceeds the length of the genome in the
+        // case that the sequence type is circular
+        is_bound integer
+        // Reserved for future use
+        evalue real
+        // Reserved for future use
+        cluster_index integer
+    }
+    
+    Table neighbors {
+        // Primary key, unique to this table
+        sort_key integer [primary key]
+        // UniProt ID
+        accession varchar(20)
+        // The number on the genome
+        num integer
+        // Pfam family ID(s)
+        family text
+        // InterPro family ID(s)
+        ipro_family text
+        // Start codon of the AA sequence on the genome
+        start integer
+        // End codon of the AA sequence on the genome
+        stop integer
+        // Start codon, but relative to the start of the primary sequence
+        // that this is related to; if it is to the left of the primary
+        // sequence then it will be negative, if to the right, then positive
+        rel_start integer
+        // End codon, but relative to the start of the primary sequence
+        // that this is related to; if it is to the left of the primary
+        // sequence then it will be negative, if to the right, then
+        // positive.  It is equal to rel_start + seq_len
+        rel_stop integer
+        // Direction of the sequence, either 'normal' or 'complement'
+        direction varchar(10)
+        // Type of the sequence, either 'linear' or 'circular'
+        type varchar(8)
+        // Length of the sequence
+        seq_len integer
+        // Taxonomy identifier as provided by NCBI
+        taxon_id integer
+        // SwissProt status; 1 if the sequence is a SwissProt sequence, 0 if TrEMBL
+        anno_status integer
+        // Sequence description if SwissProt
+        desc text
+        // Pfam family description(s)
+        family_desc text
+        // InterPro family description(s)
+        ipro_family_desc text
+        // Sequence color, based on Pfam
+        color varchar(255)
+        // Many-to-one: many neighbors can have the same gene_key, all pointing to one
+        // sort_key value in the attributes table.
+        gene_key integer
+    }
+
 
 =cut
 
