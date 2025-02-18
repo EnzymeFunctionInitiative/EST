c2cd00053808cc46e3f4ee2c268b42f754571315
Nils Oberg <noberg@illinois.edu>
2025-02-18 11:00:24 -0600

    Exit GND creation script if EFI database connection failed

--- pipelines/gnd/create_gnd.pl
+++ pipelines/gnd/create_gnd.pl
@@ -26,6 +26,12 @@ my $opts = validateAndProcessOptions(DEFAULT_NEIGHBORHOOD_SIZE);
 my $db = new EFI::Database(config => $opts->{config}, db_name => $opts->{db_name});
 my $dbh = $db->getHandle();
 die "Invalid database $opts->{db_name}" if not $dbh;
+if (not $dbh) {
+    die "Error connecting to database: " . $db->getError() . "\n";
+}
+
+
+
 
 my $idMap = parse_cluster_map_file($opts->{cluster_map});
 
