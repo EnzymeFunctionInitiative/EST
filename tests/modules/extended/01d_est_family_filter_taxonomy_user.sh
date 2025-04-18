c38f288afbfaec76f724031b19c4c57188344b52
Nils Oberg <noberg@illinois.edu>
2025-04-18 13:02:48 -0500

    Reorganize tests to distinguish between deployment and development tests

--- tests/modules/extended/01d_est_family_filter_taxonomy_user.sh
+++ tests/modules/extended/01d_est_family_filter_taxonomy_user.sh
@@ -10,10 +10,13 @@ self=$(basename "$0" .sh)
 OUTPUT_DIR="$TEST_RESULTS_DIR/$self"
 
 rm -rf $OUTPUT_DIR
+mkdir $OUTPUT_DIR
 
 family=$(<$EFI_TEST_FAMILY_ID)
 
-cat <<JSON > $OUTPUT_DIR/user_filter.json
+user_filter="$TEST_RESULTS_DIR/${self}_user_filter.json"
+
+cat <<JSON > $user_filter
 [
     {
         "name": "bacteria",
@@ -28,7 +31,7 @@ cat <<JSON > $OUTPUT_DIR/user_filter.json
 ]
 JSON
 
-./bin/create_est_nextflow_params.py family --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --fasta-db $EFI_FASTA_DB --efi-db $EFI_DB_NAME --families $family --sequence-version uniprot --nextflow-config $NXF_EST_CONFIG_FILE --filter user-file=$PWD/assets/predefined_taxonomy_filters.yml
+./bin/create_est_nextflow_params.py family --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --fasta-db $EFI_FASTA_DB --efi-db $EFI_DB_NAME --families $family --sequence-version uniprot --nextflow-config $NXF_EST_CONFIG_FILE --filter user-file=$user_filter
 bash $OUTPUT_DIR/run_nextflow.sh
 
 ./bin/create_generatessn_nextflow_params.py auto --filter-min-val 87 --ssn-name testssn --ssn-title test-ssn --est-output-dir $OUTPUT_DIR --nextflow-config $NXF_SSN_CONFIG_FILE --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME
