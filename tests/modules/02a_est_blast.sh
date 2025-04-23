2c790687e67056d31771e0d9e1dedec905225d8a
Russell Davidson <rbdavid@illinois.edu>
2025-04-23 13:16:37 -0500

    use --import-blast-* parameters explicitly here to speed up test and demonstrate usage

--- tests/modules/02a_est_blast.sh
+++ tests/modules/02a_est_blast.sh
@@ -11,7 +11,7 @@ OUTPUT_DIR="$TEST_RESULTS_DIR/$self"
 
 rm -rf $OUTPUT_DIR
 
-./bin/create_est_nextflow_params.py blast --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --fasta-db $EFI_FASTA_DB --efi-db $EFI_DB_NAME --blast-query-file $EFI_TEST_BLAST_SEQ --nextflow-config $NXF_EST_CONFIG_FILE
+./bin/create_est_nextflow_params.py blast --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --fasta-db $EFI_FASTA_DB --efi-db $EFI_DB_NAME --blast-query-file $EFI_TEST_BLAST_SEQ --nextflow-config $NXF_EST_CONFIG_FILE --import-blast-num-matches 250 --import-blast-evalue 1e-5
 bash $OUTPUT_DIR/run_nextflow.sh
 
 ./bin/create_generatessn_nextflow_params.py auto --filter-min-val 87 --ssn-name testssn --ssn-title test-ssn --est-output-dir $OUTPUT_DIR --nextflow-config $NXF_SSN_CONFIG_FILE --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME
