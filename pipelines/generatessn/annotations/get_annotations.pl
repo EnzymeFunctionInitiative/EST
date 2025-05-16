4a9737326b000feb3f518d7d1e6f6808f0fd4e2e
Nils Oberg <noberg@illinois.edu>
2025-05-16 11:53:24 -0500

    Update coments for clarity

--- pipelines/generatessn/annotations/get_annotations.pl
+++ pipelines/generatessn/annotations/get_annotations.pl
@@ -96,21 +96,27 @@ my %unirefClusterIdSeqLen;
 foreach my $accession (sort @$accessions){
     next if is_unknown_sequence($accession);
 
-    # If we are using UniRef, we need to get the attributes for all of the IDs in the UniRef seed
-    # sequence cluster.  This code does that.
-    my @sql_parts;
-    @sql_parts = $anno->build_query_string($accession);
-    push @sql_parts, getUnirefQuerySql($accession);
+    my $accessionSql = $anno->build_query_string($accession);
 
+    # If we are using UniRef, get the attributes for all of the IDs in the UniRef cluster
+    my @unirefSql;
+    if ($unirefVersion and $clusterField) {
+        @unirefSql = getUnirefQuerySql($accession);
+    }
+
+    # There will be at least one SQL statement, for the accession, and there may be additional
+    # SQL queries for IDs in the UniRef cluster, if the input is UniRef
     my @rows;
-
-    foreach my $sql (@sql_parts) {
+    foreach my $sql ($accessionSql, @unirefSql) {
+        # %unirefIds and %unirefClusterIdSeqLen are updated in queryDatabase
         my @queryRows = queryDatabase($accession, $sql, \%unirefIds, \%unirefClusterIdSeqLen);
         push @rows, @queryRows;
     }
 
+    # Get any associated RefSeq or other IDs that are mapped to the UniProt ID
     my @ncbiIds = getNcbiIds($accession);
-    
+
+    # Get a data structure that is used to save to metadata file
     my $data = formatAnnoData($accession, \@rows, \@ncbiIds, \%unirefIds, \%unirefClusterIdSeqLen);
 
     $outputIds->addSequence($accession, $data);
@@ -142,6 +148,23 @@ $dbh->disconnect();
 
 
 
+#
+# formatAnnoData
+#
+# Collects the various annotations into a form that is used by EFI::Sequence as sequence metadata
+# and ultimately as node attributes in a SSN.
+#
+# Parameters:
+#    $accession - UniProt or UniRef ID
+#    $rows - array ref of all of the annotations associated with the ID; if UniRef there will be
+#        more than one row
+#    $ncbiIds - array ref of NCBI IDs that are associated with the ID (ID only)
+#    $unirefIds - array ref of sequence IDs in the UniRef cluster
+#    $unirefClusterIdSeqLen - array ref of sequence lengths for each ID in the UniRef cluster
+#
+# Returns:
+#    hash ref of fields (usually from EFI::Annotations::Fields) mapped to values
+#
 sub formatAnnoData {
     my $accession = shift;
     my $rows = shift;
@@ -157,13 +180,19 @@ sub formatAnnoData {
 
     # Add any existing metadata attributes in to the data for the ID
     foreach my $field (@$metaAttrs) {
+        # Set the UniRef cluster sequence IDs
         if ($field eq $clusterField) {
             my @ids = map { $_->[0] } @{$unirefIds->{$accession}};
             $data->{$field} = join(",", $accession, @ids);
+
+        # Set the UniRef cluster size field
         } elsif ($field eq $clusterSizeField) {
-            my $size = scalar(map { $_->[1] } @{$unirefIds->{$accession}}) + 1; # + for the seed sequence
+            my $size = scalar(map { $_->[1] } @{$unirefIds->{$accession}}) + 1; # + 1 for the seed sequence
             $data->{$field} = $size;
             $data->{&FIELD_UNIREF_CLUSTER_ID_SEQ_LEN_KEY} = $unirefClusterIdSeqLen->{$accession} if $unirefClusterIdSeqLen->{$accession};
+
+        # If the field doesn't exist in the database attributes but exists in the existing metadata
+        # then use the existing value
         } elsif (not $data->{$field}) {
             my $value = $inputIds->getSequence($accession)->getAttribute($field);
             $data->{$field} = $value;
@@ -185,10 +214,32 @@ sub getUnirefLenFiltWhere {
 }
 
 
+#
+# getNcbiIds
+#
+# Get any known NCBI IDs that map to the input UniProt ID.  This code currently does not retrieve
+# the NCBI IDs for members of UniRef clusters, just the primary sequence ID.
+#
+# Parameters:
+#    $accession - sequence ID
+#
+# Returns:
+#    list of NCBI IDs, prefixed with the ID type
+#
+# Example Usage:
+#
+#       my @ids = getNcbiIds("B0SS77");
+#       # @ids is:
+#       # (
+#       #     "GI:229485962",
+#       #     "GI:167779669",
+#       #     "GI:501357279",
+#       #     "EMBL-CDS:ABZ97967.1",
+#       #     "REFSEQ:WP_012388845.1"
+#       # )
+#
 sub getNcbiIds {
     my $accession = shift;
-
-    #TODO: handle uniref cluster seqeuences ncbi ids
 
     my @ncbiIds;
 
@@ -208,6 +259,31 @@ sub getNcbiIds {
 }
 
 
+#
+# queryDatabase
+#
+# Retrieve the annotations (metadata) from the EFI database for a given sequence.  Multiple sets
+# of annotations can be returned for a given sequence ID when the ID represents a UniRef ID --
+# in that case each value in the returned array corresponds to a hash ref of metadata for each
+# sequence in the UniRef cluster.
+#
+# Parameters:
+#    $accession - sequence ID
+#    $sql - SQL statement to use (comes from EFI::Annotations::build_query_string)
+#    $unirefIds - hash ref that is updated with all of the IDs of the UniRef cluster
+#    $unirefClusterIdSequenceLen - hash ref that maps input sequence ID to sequence length
+#
+# Returns:
+#    array of metadata, with each element being metadata for a sequence; if UniRef, the
+#        first element corresponds to the UniRef ID
+#
+# Notes:
+#
+# The input $unirefIds structure contains a mapping of UniRef ID to list of sequence IDs that are
+# members of the UniRef cluster.  Each element in the list is an array ref of two elements, with
+# the first being the sequence ID and the second being the sequence length.  This structure is
+# updated in this function rather than being returned as a value.
+#
 sub queryDatabase {
     my $accession = shift;
     my $sql = shift;
@@ -241,13 +317,11 @@ sub queryDatabase {
 sub getUnirefQuerySql {
     my $accession = shift;
 
-    return () if (not $unirefVersion or not $clusterField);
-
-    my $value = $inputIds->getSequence($accession)->getAttribute($clusterField);
-    return () if not $value;
+    my $clusterIds = $inputIds->getSequence($accession)->getAttribute($clusterField);
+    return () if not $clusterIds;
 
     my @sql;
-    my @allIds = split(m/,/, );
+    my @allIds = split(m/,/, $clusterIds);
     my @idList = grep(!m/^$accession$/, @allIds); #remove main accession ID
     while (my @chunk = splice(@idList, 0, 200)) {
         my $sql = $anno->build_query_string(\@chunk, $unirefLenFiltWhere);
@@ -258,6 +332,20 @@ sub getUnirefQuerySql {
 }
 
 
+#
+# readAnnoSpec
+#
+# Read the annotation specification file, if it exists.  This file specifies the attributes that
+# are to be used when generating the SSN.  If not given to the script as an argument, then all
+# attributes are used.
+#
+# Parameters:
+#    $file - path to a file; can be undef
+#
+# Returns:
+#    0 if the file is not specified or doesn't exist,
+#    hash ref with keys being the attribute keys to use
+#
 sub readAnnoSpec {
     my $file = shift;
     return 0 if not $file or not -f $file;
