55caa80f73f1e760f5fe688f4bfbf4391684445f
Nils Oberg <noberg@illinois.edu>
2025-08-22 15:46:58 -0500

    Merged all config files into one for each platform.
    This required updating all of the test module files to use a new config file path.

--- tests/modules/09_gnt.sh
+++ tests/modules/09_gnt.sh
@@ -8,13 +8,12 @@ fi
 
 TEST_RESULTS_DIR=$1
 CONFIG_FILE=$2
-NXF_GNT_CONFIG_FILE="conf/gnt/$CONFIG_FILE"
 
 self=$(basename "$0" .sh)
 OUTPUT_DIR="$TEST_RESULTS_DIR/$self"
 
 rm -rf $OUTPUT_DIR
 
-./bin/create_gnt_nextflow_params.py --output-dir $OUTPUT_DIR --ssn-input $EFI_TEST_SSN_UNIPROT --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME --fasta-db $EFI_FASTA_DB --nextflow-config $NXF_GNT_CONFIG_FILE
+./bin/create_gnt_nextflow_params.py --output-dir $OUTPUT_DIR --ssn-input $EFI_TEST_SSN_UNIPROT --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME --fasta-db $EFI_FASTA_DB --nextflow-config $CONFIG_FILE
 bash $OUTPUT_DIR/run_nextflow.sh
 
