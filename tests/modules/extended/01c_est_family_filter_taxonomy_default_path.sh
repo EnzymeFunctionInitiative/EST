7d474e352ac8b781733c2e5cde6447dd21f482cf
Nils Oberg <noberg@illinois.edu>
2025-08-25 15:41:01 -0500

    Update extended tests to reflect config file changes

--- tests/modules/extended/01c_est_family_filter_taxonomy_default_path.sh
+++ tests/modules/extended/01c_est_family_filter_taxonomy_default_path.sh
@@ -3,8 +3,6 @@ set -e
 
 TEST_RESULTS_DIR=$1
 CONFIG_FILE=$2
-NXF_EST_CONFIG_FILE="conf/est/$CONFIG_FILE"
-NXF_SSN_CONFIG_FILE="conf/generatessn/$CONFIG_FILE"
 
 self=$(basename "$0" .sh)
 OUTPUT_DIR="$TEST_RESULTS_DIR/$self"
@@ -13,9 +11,9 @@ rm -rf $OUTPUT_DIR
 
 family=$(<$EFI_TEST_FAMILY_ID)
 
-./bin/create_est_nextflow_params.py family --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --fasta-db $EFI_FASTA_DB --efi-db $EFI_DB_NAME --families $family --sequence-version uniprot --nextflow-config $NXF_EST_CONFIG_FILE --filter predef-filter=bacteria
+./bin/create_est_nextflow_params.py family --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --fasta-db $EFI_FASTA_DB --efi-db $EFI_DB_NAME --families $family --sequence-version uniprot --nextflow-config $CONFIG_FILE --filter predef-filter=bacteria
 bash $OUTPUT_DIR/run_nextflow.sh
 
-./bin/create_generatessn_nextflow_params.py auto --filter-min-val 87 --ssn-name testssn --ssn-title test-ssn --est-output-dir $OUTPUT_DIR --nextflow-config $NXF_SSN_CONFIG_FILE --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME
+./bin/create_generatessn_nextflow_params.py auto --filter-min-val 87 --ssn-name testssn --ssn-title test-ssn --est-output-dir $OUTPUT_DIR --nextflow-config $CONFIG_FILE --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME
 bash $OUTPUT_DIR/ssn/run_nextflow.sh
 
