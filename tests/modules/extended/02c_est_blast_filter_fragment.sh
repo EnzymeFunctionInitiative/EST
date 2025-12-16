badb5fa14efee7d37a940d29131c3ad530b5fb70
Russell Davidson <rbdavid@illinois.edu>
2025-12-16 17:32:17 -0600

    Implement the singular input parameter and apply it to the testing modules

--- tests/modules/extended/02c_est_blast_filter_fragment.sh
+++ tests/modules/extended/02c_est_blast_filter_fragment.sh
@@ -9,7 +9,7 @@ OUTPUT_DIR="$TEST_RESULTS_DIR/$self"
 
 rm -rf $OUTPUT_DIR
 
-./bin/create_est_nextflow_params.py blast --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --fasta-db $EFI_FASTA_DB --efi-db $EFI_DB_NAME --blast-query-file $EFI_TEST_BLAST_SEQ --nextflow-config $CONFIG_FILE --filter fragments
+./bin/create_est_nextflow_params.py blast --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --fasta-db $EFI_FASTA_DB --efi-db $EFI_DB_NAME --input-file $EFI_TEST_BLAST_SEQ --nextflow-config $CONFIG_FILE --filter fragments
 bash $OUTPUT_DIR/run_nextflow.sh
 
 ./bin/create_generatessn_nextflow_params.py auto --filter-min-val 87 --ssn-name testssn --job-name test-ssn --est-output-dir $OUTPUT_DIR --nextflow-config $CONFIG_FILE --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME
