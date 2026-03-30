5c562c5aac5dd5f7915f53d5a9229c8b12f41c42
Russell Davidson <rbdavid@illinois.edu>
2026-03-30 09:53:44 -0500

    Update testing modules for Taxonomy Tool

---
+++ tests/modules/16c_taxonomy_families_filter_seq_length.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+TEST_RESULTS_DIR=$1
+CONFIG_FILE=$2
+
+self=$(basename "$0" .sh)
+OUTPUT_DIR="$TEST_RESULTS_DIR/$self"
+
+rm -rf $OUTPUT_DIR
+
+family=$(<$EFI_TEST_FAMILY_ID)
+
+./bin/create_taxonomy_nextflow_params.py family --output-dir $OUTPUT_DIR --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME --families $family --sequence-version uniprot --nextflow-config $CONFIG_FILE --filter min_seq_length=10 --filter max_seq_length=150
+bash $OUTPUT_DIR/run_nextflow.sh
+
