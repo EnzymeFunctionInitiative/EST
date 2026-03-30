625cbd4b303fff8a742032b8d0f85a546b8fdc76
Russell Davidson <rbdavid@illinois.edu>
2026-03-30 16:42:23 -0500

    Add a sunburst processing block to gather the important stats values to report

---
+++ pipelines/taxonomy/statistics/import_stats.py
@@ -0,0 +1,49 @@
+import argparse
+import json
+import os
+
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Process taxonomy tool stats.")
+    parser.add_argument("--sunburst-file", type=str, required=True, help="Path to the sunburst data file")
+    parser.add_argument("--stats-file", type=str, required=True, help="Path to the import stats file")
+    parser.add_argument("--output", type=str, required=True, help="Desired output filename")
+
+    return parser
+
+def check_args(args: argparse.Namespace) -> argparse.Namespace:
+    fail = False
+    if not os.path.exists(args.sunburst_file):
+        print(f"BLAST output '{args.sunburst_file}' does not exist")
+        fail = True
+    if not os.path.exists(args.stats_file):
+        print(f"FASTA file '{args.stats_file}' does not exist")
+        fail = True
+    
+    if fail:
+        exit(1)
+    else:
+        args.sunburst_file = os.path.abspath(args.sunburst_file)
+        args.stats_file = os.path.abspath(args.stats_file)
+        return args
+
+
+if __name__ == "__main__":
+    args = check_args(create_parser().parse_args())
+    with open(args.sunburst_file, "r") as f:
+        sunburst_data = json.load(f)
+
+    num_seqs = sunburst_data["data"]["nq"]
+
+    with open(args.stats_file, "r") as f:
+        import_stats = json.load(f)
+
+    import_stats.update(
+        {
+            "num_computed_ids": num_seqs
+        }
+    )
+
+    with open(args.output, "w") as f:
+        json.dump(output, f, indent=4)
+        f.write("\n")
+
--- pipelines/taxonomy/taxonomy.nf
+++ pipelines/taxonomy/taxonomy.nf
@@ -2,6 +2,19 @@
 include { get_source_ids; filter_ids; get_sunburst_data } from "../est/subworkflows/import.nf"
 include { merge_stats } from "../shared/nextflow/util.nf"
 
+process process_sunburst_stats {
+    publishDir params.final_output_dir, mode: 'copy'
+    input:
+        path sunburst_tax_file
+        path import_stats_file
+    output:
+        path 'sunburst_stats.json', emit: 'stats_file'
+    script:
+    """
+    python $projectDir/statistics/import_stats.py --sunburst-file ${sunburst_tax_file} --stats-file ${import_stats_file} --output sunburst_stats.json
+    """
+}
+
 workflow {
 
         // We get sequence IDs and basic metadata from the input source, including those in FASTA files
@@ -11,10 +24,13 @@ workflow {
         sequence_id_files = filter_ids(source_data.source_ids, source_data.source_meta, source_data.source_stats)
 
         // Get sunburst data for all sequence IDs, after filtering
-        get_sunburst_data(sequence_id_files.accession_table, sequence_id_files.sequence_metadata)
+        sunburst_file = get_sunburst_data(sequence_id_files.accession_table, sequence_id_files.sequence_metadata)
         
+        // Process the sunburst file and import_stats.json files
+        stats = process_sunburst_stats(sunburst_file, sequence_id_files.import_stats)
+
         // Create the stats.json file
-        final_stats = merge_stats(sequence_id_files.import_stats)
+        final_stats = merge_stats(stats.stats_file)
 
     emit:
         final_stats
