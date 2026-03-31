a856044dd86ac004b0c55ac38588c22e0e493aca
Russell Davidson <rbdavid@illinois.edu>
2026-03-31 14:29:26 -0500

    Implement logic to avoid always running the sequence length filter

--- pipelines/est/import/filter_ids.pl
+++ pipelines/est/import/filter_ids.pl
@@ -23,6 +23,8 @@ use EFI::Sequence::Collection;
 my $defaultPredefTaxFiltFileName = "predefined_taxonomy_filters.yml";
 my $defaultPredefTaxFiltFile = "$FindBin::Bin/../../shared/assets/$defaultPredefTaxFiltFileName";
 
+my $defaultMinSeqLength = EFI::Import::Config::Filter::DEFAULT_MIN_SEQ_LENGTH;
+my $defaultMaxSeqLength = EFI::Import::Config::Filter::DEFAULT_MAX_SEQ_LENGTH;
 
 my $optionParser = new EFI::Import::Config::Filter(predef_filter_file => $defaultPredefTaxFiltFile);
 my ($status, $help) = $optionParser->validateOptions();
@@ -94,20 +96,16 @@ if ($opts->{family_filter}) {
 
 # Sequence Length: Restrict sequence lengths to the given range (only applies to Taxonomy Families)
 if ($opts->{min_seq_length} or $opts->{max_seq_length}) {
-    my %args;
-    if ($opts->{min_seq_length}) {
+    # values for min/max are set during validateOptions() call
+    # check that the set values are not both equal to defaults; if they are, do not apply filter at all.
+    if ($opts->{min_seq_length} != $defaultMinSeqLength or $opts->{max_seq_length} != $defaultMaxSeqLength) {
+        my %args;
         $args{min_seq_length} = $opts->{min_seq_length};
-    } else {
-        $args{min_seq_length} = 1;
-    }
-    if ($opts->{max_seq_length}) {
         $args{max_seq_length} = $opts->{max_seq_length};
-    } else {
-        $args{max_seq_length} = 65000;
-    }
 
-    my $lengthFilter = new EFI::Import::Filter::Length(%defaultFilterArgs, %args);
-    $lengthFilter->applyFilter($seqData);
+        my $lengthFilter = new EFI::Import::Filter::Length(%defaultFilterArgs, %args);
+        $lengthFilter->applyFilter($seqData);
+    }
 }
 
 
