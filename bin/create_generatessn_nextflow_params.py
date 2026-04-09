6c79761b7325c694b73937c42d35e2abdf1a4a30
Nils Oberg <noberg@illinois.edu>
2026-04-09 16:47:18 -0500

    Add filter support to generate SSN params creation

--- bin/create_generatessn_nextflow_params.py
+++ bin/create_generatessn_nextflow_params.py
@@ -16,11 +16,11 @@ def add_args(parser):
     ssn_args_parser = argparse.ArgumentParser(add_help=False).add_argument_group("SSN Creation Options")
     ssn_args_parser.add_argument("--threshold-metric", choices=["pident", "alignment_length", "bitscore", "query_length", "subject_length", "alignment_score"], default="alignment_score", help="Parameter to filter edges on")
     ssn_args_parser.add_argument("--threshold-min-val", required=True, type=float, help="Retain rows where threshold metric >= this value")
+    ssn_args_parser.add_argument("--filter", action="append", type=str, help="Filter sequences, use multiple times to indicate filter types")
     ssn_args_parser.add_argument("--min-length", help="Minimum required sequence length")
     ssn_args_parser.add_argument("--max-length", help="Maximum sequence length to allow")
     ssn_args_parser.add_argument("--ssn-name", required=True, type=str, help="Name for the SSN file")
     ssn_args_parser.add_argument("--job-name", required=True, help="Title to be included as metadata in the XGMML file")
-#    ssn_args_parser.add_argument("--
     ssn_args_parser.add_argument("--maxfull", default=0)
 
     # Add a subparser for automatically populating from EST output dir
@@ -36,6 +36,7 @@ def add_args(parser):
     manual_parser.add_argument("--blast-parquet", required=True, type=str, help="Parquet file representing edges from EST pipeline, usually called 1.out.parquet")
     manual_parser.add_argument("--fasta-file", required=True, type=str, help="FASTA file to create SSN from")
     manual_parser.add_argument("--seq-meta-file", required=True, type=str, help="EST sequence metadata file to get basic metadata from")
+    manual_parser.add_argument("--source-ids-file", required=True, type=str, help="EST accession ID table file to get IDs from, for filtering")
     manual_parser.add_argument("--db-version", default=100, help="Indicates the version of the EFI database that was used to generate the network")
     shared_args.add_args(manual_parser)
 
@@ -54,6 +55,7 @@ def check_args(args: argparse.Namespace) -> argparse.Namespace:
         args.fasta_file = os.path.join(args.est_output_dir, "all_sequences.fasta")
         args.output_dir = os.path.join(args.est_output_dir, f"ssn")
         args.seq_meta_file = os.path.join(args.est_output_dir, "sequence_metadata.tab")
+        args.source_ids_file = os.path.join(args.est_output_dir, "accession_table.tab")
         parameter_file = os.path.join(args.est_output_dir, "params.yml")
         try:
             with open(parameter_file) as f:
@@ -82,12 +84,21 @@ def check_args(args: argparse.Namespace) -> argparse.Namespace:
         print(f"Sequence metadata file '{args.seq_meta_file}' does not exist")
         fail = True
 
+    if not os.path.exists(args.source_ids_file):
+        print(f"Source accession IDs table file '{args.source_ids_file}' does not exist")
+        fail = True
+
     # Check for shared args validity
     validated_args = shared_args.check_args(args)
     if validated_args is None:
         fail = True
     else:
         args = validated_args
+
+    # Need to store this as 'sequence_filter' because 'filter' is a reserved keyword in some
+    # contexts and can't be used in a function definition
+    args.sequence_filter = args.filter
+    del args.filter
 
     if args.workflow_def is None:
         args.workflow_def = os.path.abspath(NXF_SCRIPT)
@@ -99,6 +110,7 @@ def check_args(args: argparse.Namespace) -> argparse.Namespace:
         args.blast_parquet = os.path.abspath(args.blast_parquet)
         args.fasta_file = os.path.abspath(args.fasta_file)
         args.seq_meta_file = os.path.abspath(args.seq_meta_file)
+        args.source_ids_file = os.path.abspath(args.source_ids_file)
         return args
 
 def create_parser():
@@ -106,26 +118,29 @@ def create_parser():
     add_args(parser)
     return parser
 
-def render_params(blast_parquet, fasta_file, seq_meta_file, output_dir, efi_config,
-        db_version, job_id, efi_db, mode, ssn_name, job_name, maxfull, threshold_min_val,
-        threshold_metric=None, min_length=None, max_length=None,
+def render_params(blast_parquet, fasta_file, source_ids_file, seq_meta_file, output_dir,
+        efi_config, db_version, job_id, efi_db, mode, ssn_name, job_name, maxfull,
+        threshold_min_val,
+        threshold_metric=None, min_length=None, max_length=None, sequence_filter=None,
         **kwargs: dict):
     params = {
         "blast_parquet": blast_parquet,
         "fasta_file": fasta_file,
+        "source_ids_file": source_ids_file,
         "seq_meta_file": seq_meta_file,
         "final_output_dir": output_dir,
         "threshold_metric": threshold_metric,
         "threshold_min_val": threshold_min_val,
         "min_length": min_length,
         "max_length": max_length,
+        "filter": sequence_filter,
         "ssn_name": ssn_name,
         "job_name": job_name,
         "maxfull": maxfull,
         "efi_config": efi_config,
         "db_version": db_version,
         "job_id": job_id,
-        "efi_db": efi_db
+        "efi_db": efi_db,
     }
     
     # Handle kwargs dict, assuming each entry is a parameter to be added to params
