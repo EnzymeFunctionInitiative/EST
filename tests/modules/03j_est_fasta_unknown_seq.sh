cb71888f2e987288440ddb6e30694858695354a7
Nils Oberg <noberg@illinois.edu>
2026-05-15 14:55:51 -0500

    Fix errors in unknown fasta seq test

--- tests/modules/03j_est_fasta_unknown_seq.sh
+++ tests/modules/03j_est_fasta_unknown_seq.sh
@@ -34,18 +34,24 @@ bash $OUTPUT_DIR/run_nextflow.sh
 ./bin/create_generatessn_nextflow_params.py auto --threshold-min-val 87 --ssn-name testssn --job-name test-ssn --est-output-dir $OUTPUT_DIR --nextflow-config $CONFIG_FILE --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME
 bash $OUTPUT_DIR/ssn/run_nextflow.sh
 
-if ! grep -q ZZZZZ $fasta_file; then
+ssn_file=$(mktemp)
+exit_code=0
+unzip -c $OUTPUT_DIR/ssn/full_ssn.xgmml.zip > "$ssn_file"
+if ! grep -q ZZZZZ "$ssn_file"; then
     printf "\033[0;31mFAILED test; no unknown sequences detected\033[0m\n"
-elif ! grep -q TEST1_ID $fasta_file; then
+    exit_code=1
+elif ! grep -q TEST1_ID "$ssn_file"; then
     printf "\033[0;31mFAILED test; raw ID not present\033[0m\n"
-elif ! grep -q HCJ58040 $fasta_file; then
+    exit_code=1
+elif ! grep -q HCJ58040 "$ssn_file"; then
     printf "\033[0;31mFAILED test; metadata not present\033[0m\n"
+    exit_code=1
 else
-    print "Test passed\n"
-    rm $fasta_file
-    exit 0
+    printf "\033[0;32mTest passed\033[0m\n"
+    exit_code=0
 fi
 
 rm $fasta_file
-exit 1
+rm "$ssn_file"
+exit $exit_code
 
