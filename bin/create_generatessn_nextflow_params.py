c808fa3aaf694ffcef65d0985321f44015a7600f
Nils Oberg <noberg@illinois.edu>
2026-06-12 13:17:45 -0500

    Add create NC factor flag to generatessn params creation script

--- bin/create_generatessn_nextflow_params.py
+++ bin/create_generatessn_nextflow_params.py
@@ -23,6 +23,7 @@ def add_args(parser):
     ssn_args_parser.add_argument("--job-name", required=True, help="Title to be included as metadata in the XGMML file")
     ssn_args_parser.add_argument("--maxfull", default=0)
     ssn_args_parser.add_argument("--color-ssn", action="store_true", help="Numbers and colors the clusters in the SSN")
+    ssn_args_parser.add_argument("--compute-ssn-nc-factor", action="store_true", help="Numbers and colors the clusters in the SSN")
 
     # Add a subparser for automatically populating from EST output dir
     subparsers = parser.add_subparsers(dest="mode", required=True)
@@ -123,7 +124,7 @@ def render_params(blast_parquet, fasta_file, source_ids_file, seq_meta_file, out
         efi_config, db_version, job_id, efi_db, mode, ssn_name, job_name, maxfull,
         threshold_min_val,
         threshold_metric=None, min_length=None, max_length=None, sequence_filter=None,
-        color_ssn=False, **kwargs: dict):
+        color_ssn=False, compute_ssn_nc_factor=False, **kwargs: dict):
     params = {
         "blast_parquet": blast_parquet,
         "fasta_file": fasta_file,
@@ -143,6 +144,7 @@ def render_params(blast_parquet, fasta_file, source_ids_file, seq_meta_file, out
         "job_id": job_id,
         "efi_db": efi_db,
         "color_ssn": color_ssn,
+        "compute_ssn_nc_factor": compute_ssn_nc_factor,
     }
     
     # Handle kwargs dict, assuming each entry is a parameter to be added to params
