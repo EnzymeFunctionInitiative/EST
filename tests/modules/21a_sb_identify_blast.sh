ed6a5ba969ce04323df0d605d009d85980288c3a
rbdavid <rbdchemistry@gmail.com>
2026-09-03 23:20:53 -0500

    Update the cgfp-related test modules and documentation in the test_env.sh script

--- tests/modules/21a_sb_identify_blast.sh
+++ tests/modules/21a_sb_identify_blast.sh
@@ -17,6 +17,7 @@ rm -rf $OUTPUT_DIR
 ./bin/create_cgfpidentify_nextflow_params.py --output-dir $OUTPUT_DIR --ssn-input $EFI_TEST_SSN_UNIPROT --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME --fasta-db $EFI_FASTA_DB --nextflow-config $CONFIG_FILE \
     --search-method blast \
     --cdhit-sid 0.85 \
-    --ref-fasta-db $EFI_FASTA_DB
+    --ref-fasta-db $EFI_FASTA_DB \
+    --shortbred-src $EFI_SHORTBRED_SRC_DIR
 bash $OUTPUT_DIR/run_nextflow.sh
 
--- tests/modules/22a_sb_quantify.sh
+++ tests/modules/22a_sb_quantify.sh
@@ -19,7 +19,8 @@ mg_ids="SRS011263,SRS011397,SRS012291,SRS014464,SRS015051,SRS016188"
 ./bin/create_cgfpidentify_nextflow_params.py --output-dir $OUTPUT_DIR --ssn-input $EFI_TEST_SSN_UNIPROT --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME --fasta-db $EFI_FASTA_DB --nextflow-config $CONFIG_FILE \
     --search-method diamond \
     --cdhit-sid 0.85 \
-    --ref-fasta-db $EFI_FASTA_DB
+    --ref-fasta-db $EFI_FASTA_DB \
+    --shortbred-src $EFI_SHORTBRED_SRC_DIR
 bash $OUTPUT_DIR/run_nextflow.sh
 
 ./bin/create_cgfpquantify_nextflow_params.py --output-dir $OUTPUT_DIR/quantify --efi-config $EFI_CONFIG_FILE --efi-db $EFI_DB_NAME --nextflow-config $CONFIG_FILE \
